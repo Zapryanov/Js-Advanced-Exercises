d475290d6befd318625e63ecea3249155bb98484
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-26 06:17:45 +0200

    feat: second task is ready

--- DOM/02.Format-the-Text/solution.js
+++ DOM/02.Format-the-Text/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById("input");
+  let output = document.getElementById("output");
+
+  let textLikeArray = input.textContent.split(". ");
+  
+  for (let i = 0; i < textLikeArray.length; i++) {
+    let sentences = textLikeArray.splice(0, 3).join(". ");
+    const p = document.createElement("p");
+    
+    // if (sentences[sentences.length - 1] !== ".") {
+    //   sentences += ".";
+    // }
+
+    // The same logic like IF-statement
+    sentences = sentences[sentences.length - 1] !== "." ? sentences += "." : sentences;
+    
+    p.textContent = sentences;
+    output.appendChild(p);
+
+    i--;
+  }
 }
