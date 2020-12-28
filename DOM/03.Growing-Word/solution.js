e9d9569fcea3bbb5612188a38001ed4593dc9c28
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-28 07:38:25 +0200

    feat: some space for readability was added

--- DOM/03.Growing-Word/solution.js
+++ DOM/03.Growing-Word/solution.js
@@ -2,13 +2,12 @@ function growingWord() {
 
   let growingText = document.querySelector("#exercise p");
   let colorsArr = ["blue", "green", "red"];
-
   // getComputedStyle не работи за Judge
 
   // let style = window.getComputedStyle(growingText, null).getPropertyValue("font-size");
   // let fontSize = parseFloat(style);
   // let size = growingText.style.fontSize = fontSize * 2;
-  
+
   let size = growingText.style.fontSize.slice(0, -2) * 2;
 
   let color = growingText.style.color;
