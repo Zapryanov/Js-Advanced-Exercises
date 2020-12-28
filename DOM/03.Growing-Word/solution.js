f304e66cc042f7bca84881a1529b1013166c17be
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-28 07:22:12 +0200

    feat: solution task 3

--- DOM/03.Growing-Word/solution.js
+++ DOM/03.Growing-Word/solution.js
@@ -1,4 +1,26 @@
 function growingWord() {
 
-  //TODO...
+  let growingText = document.querySelector("#exercise p");
+  let colorsArr = ["blue", "green", "red"];
+
+  // getComputedStyle не работи за Judge
+
+  // let style = window.getComputedStyle(growingText, null).getPropertyValue("font-size");
+  // let fontSize = parseFloat(style);
+  // let size = growingText.style.fontSize = fontSize * 2;
+  
+  let size = growingText.style.fontSize.slice(0, -2) * 2;
+
+  let color = growingText.style.color;
+  let index = colorsArr.indexOf(color);
+  
+  if (size === 0) {
+    size = 2;
+  }
+
+  let nextColor = index + 1 >= colorsArr.length ? colorsArr[0] : colorsArr[index + 1];
+
+  growingText.style.fontSize = `${size}px`;
+  growingText.style.color = nextColor;
+  
 }
