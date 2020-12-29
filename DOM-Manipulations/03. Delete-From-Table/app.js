09179912b0d79eccd2d661d743c369acc2f3a5a2
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-30 00:12:18 +0200

    feat: third task is ready

--- DOM-Manipulations/03. Delete-From-Table/app.js
+++ DOM-Manipulations/03. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const input = document.querySelector("label input");
+    const tBody = document.querySelectorAll("tbody tr");
+    const result = document.getElementById("result");
+    result.textContent = "";
+
+    if (input.value && input.value !== "") {
+        Array.from(tBody).map(x => {
+            const eMail = x.lastElementChild.textContent;
+            if (input.value === eMail) {
+                x.remove();
+                input.value = "";
+                result.textContent = "Deleted."
+            }
+        })
+    }
+    result.textContent = "Not found.";
 }
