b85cbcc23459c3c5c15aaf08cac11d8917117d9f
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-29 21:22:08 +0200

    feat: solution task-1

--- DOM-Manipulations/01. List-Of-Items/app.js
+++ DOM-Manipulations/01. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    const ul = document.getElementById("items");
+    const input = document.getElementById("newItemText");
+
+    if (input.value !== "") {
+        const newLi = document.createElement("li");
+        newLi.textContent = input.value;
+        ul.appendChild(newLi);
+        input.value = "";
+    }
 }
