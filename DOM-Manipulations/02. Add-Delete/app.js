5cec44f43aced5513d83e1e003d5d3c733ccc810
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2020-12-29 23:21:46 +0200

    feat: solution task-2

--- DOM-Manipulations/02. Add-Delete/app.js
+++ DOM-Manipulations/02. Add-Delete/app.js
@@ -1,3 +1,23 @@
 function addItem() {
-    //TODO...
+    const ul = document.getElementById("items");
+    const input = document.getElementById("newText");
+
+    if (input.value !== "") {
+        const newLi = document.createElement("li");
+        newLi.textContent = input.value;
+        
+        const aDelete = document.createElement("a");
+        aDelete.setAttribute("href", "#")
+        aDelete.textContent = "[Delete]";
+        newLi.appendChild(aDelete);
+
+        aDelete.addEventListener("click", deleteItem);
+
+        ul.appendChild(newLi);
+        input.value = "";
+    }
+
+    function deleteItem(e) {
+        e.target.parentNode.remove();
+    }
 }
