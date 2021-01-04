377de5f7ba7521d924188f23f57f3b9616109341
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-04 15:13:21 +0200

    fix: problem is resolved

--- CodeWars-Exercises/findUniq/index.js
+++ CodeWars-Exercises/findUniq/index.js
@@ -1,16 +1,4 @@
-// The fastest solution, the best performance to find
-// a single non-repeating element in an array
-
 function findUniq(arr) {
-    arr.sort((a,b) => a - b);
-    return arr[0] == arr[1] ? arr.pop() : arr[0]
-}
-
-// Second place good preformance
-
-function findUniqSecondFunction(arr) {
-
-
     var n = arr.length;
     var result = [];
 
@@ -27,7 +15,7 @@ function findUniqSecondFunction(arr) {
     console.log(nonrep())
 }
 
-findUniq([ 0, 1, 0 ]);
-findUniq([ 1, 1, 1, 2, 1, 1 ]);
-findUniq([ 3, 10, 3, 3, 3 ]);
-findUniq(["asd", "asd", "bds", "asd"])
+// findUniq([ 0, 1, 0 ]);
+// findUniq([ 1, 1, 1, 2, 1, 1 ]);
+findUniq([ 3, 10, 3, 3, 3, 100, 100 ]);
+// findUniq(["asd", "asd", "bds", "asd"])
