efc11ea02205541466d112b3fd605ce15e88b32e
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-04 11:40:14 +0200

    feat: solution validBraces

---
+++ CodeWars-Exercises/validBraces/index.js
@@ -0,0 +1,27 @@
+function validBraces(braces) {
+    let arr = [];
+    for (let i = 0; i < braces.length; i++) {
+        let el = braces[i];
+        if (el === "(" || el === "[" || el === "{") {
+            arr.push(el);
+        } else {
+            if (arr.length === 0) {
+                return false;
+            }
+            let lastValue = arr[arr.length - 1];
+            if ( (el === ")" && lastValue === "(") || (el === "]" && lastValue === "[") || (el === "}" && lastValue === "{") ) {
+                arr.pop();
+            }
+            else {
+                break;
+            }
+        }
+    }
+    console.log(arr.length === 0)
+    return arr.length === 0;
+}
+
+// validBraces("()");
+// validBraces("[(])");
+// validBraces("(){}[]");
+validBraces("{([])}{[]}")
