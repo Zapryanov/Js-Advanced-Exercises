24020deab074c08edb9b41a5ee9d119ff839bbb9
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-05 07:14:27 +0200

    feat: small changes

--- CodeWars-Exercises/expandedForm/index.js
+++ CodeWars-Exercises/expandedForm/index.js
@@ -21,15 +21,15 @@ expandedForm(12); // Should return '10 + 2'
 expandedForm(42); // Should return '40 + 2'
 expandedForm(70304); // Should return '70000 + 300 + 4'
 
-// The best solution from top student --------------------------
+// THE BEST Solution from top student --------------------------
 
-const expandedForm = n => n.toString()
-                            .split("")
-                            .reverse()
-                            .map( (a, i) => a * Math.pow(10, i))
-                            .filter(a => a > 0)
-                            .reverse()
-                            .join(" + ");
+// const expandedForm = n => n.toString()
+//                             .split("")
+//                             .reverse()
+//                             .map( (a, i) => a * Math.pow(10, i))
+//                             .filter(a => a > 0)
+//                             .reverse()
+//                             .join(" + ");
 
 // My first solution but longer ----------------------------------
 
