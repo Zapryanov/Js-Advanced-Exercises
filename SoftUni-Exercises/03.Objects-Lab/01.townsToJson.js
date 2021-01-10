ed51c19e74337d8de66ea20ea9e5c968a8798561
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-10 07:39:39 +0200

    fix: hardcoded approach was removed

--- SoftUni-Exercises/03.Objects-Lab/01.townsToJson.js
+++ SoftUni-Exercises/03.Objects-Lab/01.townsToJson.js
@@ -9,7 +9,14 @@ function townToJson(arr) {
         let [town, latitude, longitude] = cur.split(regex).filter(clearSpace);
         latitude = Math.round(latitude * 100) / 100;
         longitude = Math.round(longitude * 100) / 100;
-        let curObj = {"Town": town, "Latitude": Number(latitude), "Longitude": Number(longitude)};
+// This is hardcode approach with keys
+        // let curObj = {"Town": town, "Latitude": Number(latitude), "Longitude": Number(longitude)};
+// ----------------------------------------------------------------------------------------------------
+        let curObj = {};
+        curObj[headers[0]] = town;
+        curObj[headers[1]] = Number(latitude);
+        curObj[headers[2]] = Number(longitude);
+        
         acc.push(curObj);
         return acc;
     }, []);
--- SoftUni-Exercises/Just-Testing-Something/test-1.js
+++ SoftUni-Exercises/Just-Testing-Something/test-1.js
@@ -1,5 +1,18 @@
-function test(count = 5) {
-    console.log("*".repeat(count));
+const foo = {
+    fooProp1: 1,
+    fooProp2: 2,
+    fooProp3: 3
 }
 
-test(2)
+const bar = Object.create(foo);
+bar.barProp2 = 10;
+bar.barProp1 = 123;
+
+for (let key in bar) {
+    if (bar.hasOwnProperty(key)) {
+        console.log(key, " - ", bar[key]);
+    }
+}
+
+// console.log(foo);
+console.log(Object.keys(bar));
