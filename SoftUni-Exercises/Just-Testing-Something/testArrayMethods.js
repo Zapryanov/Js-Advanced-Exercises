657035868b3d4f3bfa7078a6e482c4161641199a
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-12 22:41:58 +0200

    feat: just exercise something

--- SoftUni-Exercises/Just-Testing-Something/testArrayMethods.js
+++ SoftUni-Exercises/Just-Testing-Something/testArrayMethods.js
@@ -1,13 +1,4 @@
-let arr1 = ["Flower", "Car", "Ball"];
-let arr2 = ["River", "Ocean", "Lake"];
-let arr3 = ["Run", "Jump", "Walk"];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-let concatArr = arr1.concat(arr2, arr3);
-let spredArr = [...arr1, ...arr2, ...arr3];
-
-console.log(concatArr);
-console.log(spredArr);
-
-console.log(arr1);
-console.log(arr2);
-console.log(arr3);
+let newArr = arr.slice(-3);
+console.log(newArr)
