84e79bfe0b95b84948c386af80498d3cc61e35ac
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-13 21:56:52 +0200

    feat: add solution without array methods in most cases

--- SoftUni-Exercises/05.Arrays-Lab/04.lastKNumbersSequence.js
+++ SoftUni-Exercises/05.Arrays-Lab/04.lastKNumbersSequence.js
@@ -8,10 +8,39 @@ function lastKNumbersSequence(n, k) {
     for (let i = 1; i < n; i++ ) {
         let sum = 0;
         let nextNums = result.slice(-k);
-        nextNums.length < k ? sum = result.reduce(agregate) : sum = nextNums.reduce(agregate);
+        if (nextNums.length < k) {
+            sum = result.reduce(agregate);
+        } else {
+            sum = nextNums.reduce(agregate);
+        }
         result.push(sum);
     }
     return result;
 }
 
 console.log(lastKNumbersSequence(6, 3));
+
+
+// function lastKNumbersSequence(n, k) {
+//     let result = [1];
+
+//     function agregate(acc, cur) {
+//         return acc + cur;
+//     }
+
+//     for (let i = 0; i < n - 1; i++ ) {
+//         let sum = 0;
+//         let nextNums = [];
+//         for (let j = k - 1; j >= 0; j-- ) {
+//             let num = result[i - j];
+//             if (num) {
+//                 nextNums.push(num);
+//             }
+//         }
+//         sum = nextNums.reduce(agregate, 0) || 1;
+//         result.push(sum);
+//     }
+//     return result;
+// }
+
+// console.log(lastKNumbersSequence(6, 3));
