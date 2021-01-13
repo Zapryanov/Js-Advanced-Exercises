59cae22ac5a761c9840979642effebe8f62f430b
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-13 22:18:54 +0200

    feat: make code more readable

--- SoftUni-Exercises/05.Arrays-Lab/05.processOddNumbers.js
+++ SoftUni-Exercises/05.Arrays-Lab/05.processOddNumbers.js
@@ -1,5 +1,9 @@
 function processOddNumbers(arr) {
-    return arr.filter((el, i) => i % 2 !== 0).map(x => x * 2).reverse().join(" ")
+    return arr
+        .filter((el, i) => i % 2 !== 0)
+        .map(x => x * 2)
+        .reverse()
+        .join(" ")
 }
 
 console.log(processOddNumbers([3, 0, 10, 4, 7, 3]))
