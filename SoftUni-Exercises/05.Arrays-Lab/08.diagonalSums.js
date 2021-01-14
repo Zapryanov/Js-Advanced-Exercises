09e0160ed4e7b2b91dc47901b0254b1c1af8c3ea
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-14 22:52:01 +0200

    feat: Martin Chaov implementation

--- SoftUni-Exercises/05.Arrays-Lab/08.diagonalSums.js
+++ SoftUni-Exercises/05.Arrays-Lab/08.diagonalSums.js
@@ -1,22 +1,11 @@
+// Better way - Martin Chaov implementation
+
 function diagonalSums(matrixTwoD) {
-    let leftDiagonal = [];
-    let rightDiagonal = [];
-
-    function sum(acc, cur) {
-        acc += cur;
-        return acc;
-    }
-
-    for (let i = 0; i < matrixTwoD.length; i++) {
-        let arr = matrixTwoD[i];
-        rightDiagonal.push(arr[arr.length - i - 1]);
-        leftDiagonal.push(arr[i]);
-    }
-
-    let leftSum = leftDiagonal.reduce(sum);
-    let rightSum = rightDiagonal.reduce(sum);
-
-    return `${leftSum} ${rightSum}`;
+    return matrixTwoD.reduce((result, row, i, arr) => {
+        result[0] += row[i];
+        result[1] += row[arr.length - 1 - i]
+        return result;
+    }, [0, 0]).join(" ")
 }
 
 console.log(diagonalSums(
@@ -26,3 +15,32 @@ console.log(diagonalSums(
         [1, -8, 89]
     ]
 ))
+
+// function diagonalSums(matrixTwoD) {
+//     let leftDiagonal = [];
+//     let rightDiagonal = [];
+
+//     function sum(acc, cur) {
+//         acc += cur;
+//         return acc;
+//     }
+
+//     for (let i = 0; i < matrixTwoD.length; i++) {
+//         let arr = matrixTwoD[i];
+//         rightDiagonal.push(arr[arr.length - i - 1]);
+//         leftDiagonal.push(arr[i]);
+//     }
+
+//     let leftSum = leftDiagonal.reduce(sum);
+//     let rightSum = rightDiagonal.reduce(sum);
+
+//     return `${leftSum} ${rightSum}`;
+// }
+
+// console.log(diagonalSums(
+//     [
+//         [3, 5, 17],
+//         [-1, 7, 14],
+//         [1, -8, 89]
+//     ]
+// ))
