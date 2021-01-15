71396375aeb9abc3c9614ebf655c3df241887b77
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-15 08:21:02 +0200

    feat: implementation with reduceRight

--- SoftUni-Exercises/06.Arrays-Exercise/04.rotateArray.js
+++ SoftUni-Exercises/06.Arrays-Exercise/04.rotateArray.js
@@ -2,12 +2,13 @@ function rotateArray(arr) {
     let copy = arr.slice(0);
     let times = Number(copy.pop()) % copy.length;
 
-    for (let i = 0; i < times; i++) {
-        let removed = copy.pop();
-        copy.unshift(removed);
-    }
-
-    return copy;
+    return copy.reduceRight((acc, cur) => {
+        if (times) {
+            acc = [cur, ...acc.slice(0, copy.length - 1)];
+            times--;
+        }
+        return acc;
+    }, [...copy])
 }
 
 console.log(rotateArray(
@@ -27,3 +28,34 @@ console.log(rotateArray(
         "15"
     ]
 ))
+
+
+// function rotateArray(arr) {
+//     let copy = arr.slice(0);
+//     let times = Number(copy.pop()) % copy.length;
+
+//     for (let i = 0; i < times; i++) {
+//         let removed = copy.pop();
+//         copy.unshift(removed);
+//     }
+
+//     return copy;
+// }
+
+// console.log(rotateArray(
+//     // [
+//     //     "1",
+//     //     "2",
+//     //     "3",
+//     //     "4",
+//     //     "2"
+//     // ]
+
+//     [
+//         "Banana",
+//         "Orange",
+//         "Coconut",
+//         "Apple",
+//         "15"
+//     ]
+// ))
