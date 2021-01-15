a79ebf3daf50de940f7b7ffa6339fa8ec3df3b7d
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-15 07:47:04 +0200

    feat: better implementationwithout If-Else

--- SoftUni-Exercises/06.Arrays-Exercise/03.addAndRemove.js
+++ SoftUni-Exercises/06.Arrays-Exercise/03.addAndRemove.js
@@ -1,19 +1,17 @@
-function addAndRemove(arr) {
-    let result = [];
+function addAndRemove(input) {
+    let command = {
+        counter: 1,
+        add: (arr, num) => [...arr, num],
+        remove: (arr) => [...arr.slice(0, arr.length - 1)]
+    }
 
-    for (let i = 0; i < arr.length; i++) {
-        let command = arr[i];
-        if (command === "add") {
-            result.push(i + 1);
-        } else {
-            result.splice(i - 1, 1);
-        }
-    }
-    if (result.length === 0) {
-        return "Empty";
-    } else {
-        return result.join("\n")
-    }
+    let result = input.reduce((acc, cur) => {
+        acc = command[cur](acc, command["counter"]);
+        command["counter"]++
+        return acc;
+    }, [])
+
+    return result.length > 0 ? result.join("\n") : "Empty";
 }
 
 console.log(addAndRemove(
@@ -30,3 +28,38 @@ console.log(addAndRemove(
     //     "remove"
     // ]
 ))
+
+// Old school solution -------------------------------------------
+
+// function addAndRemove(arr) {
+//     let result = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         let command = arr[i];
+//         if (command === "add") {
+//             result.push(i + 1);
+//         } else {
+//             result.splice(i - 1, 1);
+//         }
+//     }
+//     if (result.length === 0) {
+//         return "Empty";
+//     } else {
+//         return result.join("\n")
+//     }
+// }
+
+// console.log(addAndRemove(
+//     [
+//         "add",
+//         "add",
+//         "remove",
+//         "add",
+//         "add"
+//     ]
+//     // [
+//     //     "remove",
+//     //     "remove",
+//     //     "remove"
+//     // ]
+// ))
