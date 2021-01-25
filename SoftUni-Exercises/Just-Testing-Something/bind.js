2bc8855cc816557a60a3f4455716fb85b57d08f6
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-25 21:27:35 +0200

    feat: add few comments

--- SoftUni-Exercises/Just-Testing-Something/bind.js
+++ SoftUni-Exercises/Just-Testing-Something/bind.js
@@ -10,9 +10,13 @@ function test(msg, bye) {
     console.log(`${bye} ${this.brand}.`);
 }
 
+// This is the right way, to pass arguments after function initialization with bind
 let firstOutput = test.bind(car);
-
 firstOutput("is awesome", "See you soon");
+
+// This is hardcore way to pass an arguments during function initialization with bind
+let firstOutput2 = test.bind(car, "is awesome-2", "See you soon-2");
+firstOutput2();
 
 // This will return false, because a brand new function is created through the "bind"
 console.log(firstOutput === test);
