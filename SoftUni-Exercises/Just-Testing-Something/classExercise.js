3a788cd3140e33b31c6ca5487e890ae88f4a53ad
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-27 17:27:08 +0200

    feat: just exercise classes

---
+++ SoftUni-Exercises/Just-Testing-Something/classExercise.js
@@ -0,0 +1,38 @@
+class Person {
+
+    #currentAge;
+
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.#currentAge = age;
+    }
+
+    greet() {
+        return `Hello I am ${this.firstName} ${this.lastName} and I am ${this.age} years old!`;
+    };
+
+    get age() {
+        return this.#currentAge;
+    }
+
+    set age(value) {
+        // if (!(value < 0 || value > 120)) {
+        //     this.#currentAge = value;
+        // }
+        if (value < 0 || value > 120) {
+            return this.#currentAge = "The age must be bigger than 0 and less than 120...!!!";
+        } else {
+            return this.#currentAge = value;
+        }
+    }
+}
+
+const person1 = new Person("Stive", "Bolduin", 22);
+const person2 = new Person("Lucy", "Spenson", 33);
+
+console.log(person1.greet());
+console.log(person2.greet());
+
+person1.age = 10;
+console.log(person1.age);
