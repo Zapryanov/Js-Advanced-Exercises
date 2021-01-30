5e20b7f27ffa976a1167ad91a6e86dee6baf9222
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-30 08:30:32 +0200

    feat: solution task-3

---
+++ SoftUni-Exercises/15.Classes-Exercise/03.unity.js
@@ -0,0 +1,33 @@
+class Rat {
+    constructor(name) {
+        this.name = name;
+        this.unitedRats = [];
+    }
+
+    unite(newRat) {
+        if (newRat instanceof Rat) {
+            this.unitedRats.push(newRat);
+        }
+    }
+
+    getRats() {
+        return this.unitedRats;
+    }
+
+    toString() {
+        return `${this.name}\n${this.unitedRats.map(x => `##${x}`).join("")}`
+    }
+}
+
+let firstRat = new Rat("Peter");
+console.log(firstRat.toString()); // Peter
+console.log(firstRat.getRats()); // []
+firstRat.unite(new Rat("George"));
+firstRat.unite(new Rat("Alex"));
+console.log(firstRat.getRats());
+// [ Rat { name: &#39;George&#39;, unitedRats: [] },
+// Rat { name: &#39;Alex&#39;, unitedRats: [] } ]
+console.log(firstRat.toString());
+// Peter
+// ##George
+// ##Alex
