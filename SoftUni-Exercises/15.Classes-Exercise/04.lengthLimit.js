2bb126a508219356ea14481b6818305f839c42f6
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-01-30 11:06:13 +0200

    feat: solution task-4

---
+++ SoftUni-Exercises/15.Classes-Exercise/04.lengthLimit.js
@@ -0,0 +1,31 @@
+class Stringer {
+    constructor(innerString, innerLength) {
+        this.innerString = innerString;
+        this.innerLength = Number(innerLength);
+    }
+
+    increase(num) {
+        this.innerLength += num;
+    }
+
+    decrease(num) {
+        this.innerLength = Math.max(0, this.innerLength - num);
+    }
+
+    toString() {
+        if (this.innerString.length > this.innerLength) {
+            return this.innerString.substring(0, this.innerLength) + "...";
+        } else {
+            return this.innerString;
+        }
+    }
+}
+
+let test = new Stringer("Test", 5);
+console.log(test.toString()); // Test
+test.decrease(3);
+console.log(test.toString()); // Te...
+test.decrease(5);
+console.log(test.toString()); // ...
+test.increase(4);
+console.log(test.toString()); // Test
