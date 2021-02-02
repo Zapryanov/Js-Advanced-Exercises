2bdd3b61424b7c4c203a6cd91bc1c57fc9cccb8f
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-02-02 06:58:21 +0200

    feat: some more tests

--- SoftUni-Exercises/Just-Testing-Something/objectCompositionTests.js
+++ SoftUni-Exercises/Just-Testing-Something/objectCompositionTests.js
@@ -30,19 +30,44 @@ let showGrades = reportGrades.bind(person);
 console.log(showGrades());
 
 const { school: { teachers, maxGrade } } = person;
-console.log(teachers)
-console.log(maxGrade);
+// console.log(teachers);
+// console.log(maxGrade);
 
 
-console.log("-----------------------")
+// console.log("-----------------------");
 
 // Destructoring - Array
 
 const arr = ["Pesho", "Monica", "Stamat", "Teodor", "Alice", "Nikolina"];
 
-const [personOne, personTwo, personThree, ...others] = arr;
+const [personOne, personTwo, personThree, ...others] = arr;000
 
-console.log(personOne);
-console.log(personTwo);
-console.log(personThree);
-console.log(others);
+// console.log(personOne);
+// console.log(personTwo);
+// console.log(personThree);
+// console.log(others);
+
+// Destructoring nested Array - destructuring
+
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+let [first, second] = matrix;
+
+console.log(first, second)
+
+// Destructoring mixed objects in an array
+
+let arrWithObjects = [
+    { catName: "Navcho", catAge: 5 },
+    { catName: "Kitty", catAge: 3 },
+    { catName: "Bimbo", catAge: 4 },
+];
+
+let [ {catName, catAge}, secondObj ] = arrWithObjects;
+
+console.log(catName, catAge);
+console.log(secondObj);
