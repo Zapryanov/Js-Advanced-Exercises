47bec73c49aee0dd83243d550164173104712ec5
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-02-03 21:32:14 +0200

    feat: add code only for Judge system

--- SoftUni-Exercises/16.Object-Composition-Lab.js/05.sum.js
+++ SoftUni-Exercises/16.Object-Composition-Lab.js/05.sum.js
@@ -24,3 +24,26 @@
         numTwo.value = "";
     }
 })();
+
+// Only for Judge
+
+(function() {
+    let firstElement;
+    let secondElement;
+    let result;
+
+    return {
+        init: (selector1, selector2, resultSelector) => {
+            firstElement = document.querySelector(selector1);
+            secondElement = document.querySelector(selector2);
+            result = document.querySelector(resultSelector);
+        },
+        add: () => {
+            result.value = Number(firstElement.value) + Number(secondElement.value);
+        },
+        subtract: () => {
+            result.value = Number(firstElement.value) - Number(secondElement.value);
+
+        }
+    };
+})
