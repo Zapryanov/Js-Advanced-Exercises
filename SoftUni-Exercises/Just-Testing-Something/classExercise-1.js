8b4ac0807b5763f32439f05e9cd7d1296af67eb1
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-02-04 15:40:51 +0200

    feat: just exercise class inheritance

--- SoftUni-Exercises/Just-Testing-Something/classExercise-1.js
+++ SoftUni-Exercises/Just-Testing-Something/classExercise-1.js
@@ -1,7 +1,12 @@
 class Person {
-    constructor(gender, age) {
+    constructor(name, gender, age) {
+        this.name = name;
         this.gender = gender;
         this.age = age;
+    }
+
+    speak() {
+        console.log(`Hello, my name is ${this.name}!`);
     }
 }
 
@@ -9,15 +14,32 @@ const personInstance = new Person("man", 32);
 // console.log(personInstance);
 
 class Teacher extends Person {
-    constructor(gender, age, name, subject) {
-        super(gender, age);
-        this.name = name;
+    constructor(name, gender, age, subject) {
+        super(name, gender, age);
         this.age = age;
         this.subject = subject;
     }
 }
 
-const teacherInstance = new Teacher("woman", 25, "Antonia", "Js-programmer");
+class Student extends Teacher {
+    constructor(name, gender, grade) {
+        super(name, gender);
+        this.grade = grade;
+    }
+
+    info() {
+        console.log(`My name is ${this.name}. I'm a ${this.gender}. I'm an ${this.grade}-th grade student.`);
+    }
+}
+
+const studentInstance = new Student("Ivan", "man", 17);
+studentInstance.info();
+
+const teacherInstance = new Teacher("Antonia", "woman", 25, "Js-programmer");
 // console.log(teacherInstance);
 
 Object.entries(teacherInstance).forEach(x => console.log(`${x[0]}: ${x[1]}`));
+
+console.log("-----------------")
+
+teacherInstance.speak()
