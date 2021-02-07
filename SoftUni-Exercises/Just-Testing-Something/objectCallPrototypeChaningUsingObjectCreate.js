eb9ea35964e044ea4a8e579e672c9f25ca39f03f
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-02-07 08:19:33 +0200

    feat: small changes for readability

--- SoftUni-Exercises/Just-Testing-Something/objectCallPrototypeChaningUsingObjectCreate.js
+++ SoftUni-Exercises/Just-Testing-Something/objectCallPrototypeChaningUsingObjectCreate.js
@@ -15,12 +15,15 @@ pesho.speak();  //  Hello, my name is Pesho Georgiev.
 // ---------------- Inheritance with Built-In Function Call()----------------- //
 
 function Student(firstName, lastName, course) {
+
 // In this way we call the constructor of the parent
-    Person.call(this, firstName, lastName);    // <--- Equivalent to the class sintaxis --- SUPER...!!!
+
+    Person.call(this, firstName, lastName);    // <--- Equivalent to the class sintaxis --- Simulating SUPER...!!!
+
     this.course = course;
 }
 
-Student.prototype = Object.create(Person.prototype);    // 
+Student.prototype = Object.create(Person.prototype);    // Prototype Chaining
 
 Student.prototype.speakStudent = function(grades) {
     console.log(`Student: ${this.firstName} ${this.lastName} from ${this.course} course. His grades are ${grades}.`);
@@ -30,6 +33,7 @@ const firstStudent = new Student("Stamat", "Petrov", "Js-Advanced");
 
 console.log(firstStudent);  // Student {firstName: 'Stamat', lastName: 'Petrov', course: 'Js-Advanced'}
 firstStudent.speakStudent("5, 6, 6");   // Student: Stamat Petrov from Js-Advanced course. His grades are 5, 6, 6.
+firstStudent.speak();
 
 console.log(firstStudent instanceof Person);    // firstStudent is instance of boath - Person and Student
 console.log(firstStudent instanceof Student);   // firstStudent is instance of boath - Person and Student
