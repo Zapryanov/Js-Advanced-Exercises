f2d64381f4327ce7823c11ec3539ee21bd546fc1
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-02-16 23:25:44 +0200

    feat: new files and changes

--- SoftUni-Exercises/Project-Mimeto/index.js
+++ SoftUni-Exercises/Project-Mimeto/index.js
@@ -1,3 +1,10 @@
 (function() {
     console.log("Yes...")
+    let reasonsWrapper = document.getElementsByClassName("a-five-reasons");
+
+    console.log(reasonsWrapper);
+    let plantsArray = document.getElementsByClassName("a-five-reasons")
+
+    console.log(plantsArray)
+    
 })();
