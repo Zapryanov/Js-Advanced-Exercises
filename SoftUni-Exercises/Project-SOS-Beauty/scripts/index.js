eb2d10d7081e3c8bb639b15f3264b5e94896386c
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-03-04 08:18:31 +0200

    feat: a few changes

--- SoftUni-Exercises/Project-SOS-Beauty/scripts/index.js
+++ SoftUni-Exercises/Project-SOS-Beauty/scripts/index.js
@@ -1,10 +1,6 @@
-document.getElementById("test").addEventListener("click", (e) => {
-    console.log(e.target.textContent)
-});
-
-document.getElementById("test2").addEventListener("click", (e) => {
-    console.log(e.target.textContent)
-});
+Array.from(document.getElementsByClassName("link-nav-aside")).forEach(e => {
+    console.log(e.firstChild.textContent);
+})
 
 window.addEventListener("DOMContentLoaded", (e) => {
     console.log("It is working!")
