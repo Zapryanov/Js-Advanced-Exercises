4addc6b50e577eab20d5174a9733e8bbe43c03cd
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-03-06 20:52:33 +0200

    feat: one  more picture and to many changes

--- SoftUni-Exercises/Project-SOS-Beauty/scripts/index.js
+++ SoftUni-Exercises/Project-SOS-Beauty/scripts/index.js
@@ -1,8 +1,40 @@
-Array.from(document.getElementsByClassName("link-nav-aside")).forEach(e => {
-    console.log(e.firstChild.textContent);
+// Array.from(document.getElementsByClassName("link-nav-aside")).forEach(e => {
+//     console.log(e.firstChild.textContent);
+// })
+
+// window.addEventListener("DOMContentLoaded", (e) => {
+//     console.log("It is working!")
+//     document.getElementsByTagName("body")[0].classList.add("load")
+// })
+
+const showButtons = Array.from(document.getElementsByClassName("more"));
+
+showButtons.forEach(el => {
+
+    el.addEventListener("click", function(e) {
+
+        const x = e.target.nextElementSibling;
+    
+        if (x.style.display === "" || x.style.display === "none") {
+            x.style.display = "block";
+            x.classList.add("show");
+        }
+        
+        const close = Array.from(document.getElementsByClassName("closeIcon"));
+    
+        close.forEach(item => {
+            item.addEventListener("click", function(ev) {
+                ev.target.parentNode.style.display = "none";
+                x.classList.add("close")
+            })
+        })
+    
+        e.target.style.backgroundColor = "#00cccc";
+    
+    })
+
 })
 
-window.addEventListener("DOMContentLoaded", (e) => {
-    console.log("It is working!")
-    document.getElementsByTagName("body")[0].classList.add("load")
-})
+
+
+
