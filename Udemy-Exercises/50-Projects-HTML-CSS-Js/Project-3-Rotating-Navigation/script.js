15fb8df8305e02c58ec28a9bd4e8f19620349d9a
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-04-21 15:46:13 +0300

    feat: unnecessary comments have been removed

--- Udemy-Exercises/50-Projects-HTML-CSS-Js/Project-3-Rotating-Navigation/script.js
+++ Udemy-Exercises/50-Projects-HTML-CSS-Js/Project-3-Rotating-Navigation/script.js
@@ -3,11 +3,9 @@ const close = document.getElementById("close");
 const container = document.querySelector(".container");
 
 open.addEventListener("click", () => {
-    console.log("Open.....");
     container.classList.add("show-nav");
 });
 
 close.addEventListener("click", () => {
-    console.log("Close.....");
     container.classList.remove("show-nav");
 })
