6dda0b127cfa65c5cdb5934c33b94a491e4e7b87
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-05-21 22:47:38 +0300

    feat: css class will be added via javascript

--- SoftUni-Exercises/Project-Mimeto-New/script.js
+++ SoftUni-Exercises/Project-Mimeto-New/script.js
@@ -1,8 +1,15 @@
 const nav = document.querySelector(".navigation");
 const logo = document.querySelector(".wrap-logo");
 
+const galleryImages = document.querySelectorAll(".gallery-img");
+
+galleryImages.forEach((img, i) => {
+    i <= 2 ? img.classList.add("small-img") : img.classList.add("big-img");
+})
+
 window.addEventListener("load", () => {
     nav.classList.add("show-after-load");
     logo.classList.add("show-after-load");
     console.log(logo)
 })
+
