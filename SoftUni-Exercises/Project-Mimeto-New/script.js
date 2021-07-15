937dd901b7be6f0686630f143930e0408ab06d4f
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-07-15 23:37:41 +0300

    feat: latest changes

--- SoftUni-Exercises/Project-Mimeto-New/script.js
+++ SoftUni-Exercises/Project-Mimeto-New/script.js
@@ -9,22 +9,37 @@ galleryImages.forEach((img, i) => {
     i <= 2 ? img.classList.add("small-img") : img.classList.add("big-img");
 })
 
+// Анимация на логото
 window.addEventListener("load", () => {
-    nav.classList.add("show-after-load");
+    // nav.classList.add("show-after-load");
     logo.classList.add("show-after-load");
 })
 
+// Анимираната долна черта от навигацията
 navLinks.forEach(link => {
     if (link.textContent === title) {
         link.classList.add("show-dash");
     }
 })
 
+// Показване и скриване на повече или по-малко текст
 const textArr = document.querySelectorAll(".wrap-text-article");
+const btnArr = Array.from(document.querySelectorAll(".btn-show-text"));
 
-textArr.forEach(eachArr => {
-    // if (title === "За Мравка Занимавка") {
-        const result = eachArr.textContent.split(" ").filter(x => x !== "").splice(0, 10).join(" ");
-        eachArr.textContent = result + ".....";
-    // }
+btnArr.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        if (btn.innerText === "Виж повече") {
+            btnArr.forEach(x => {
+                if (x != e.target) {
+                    x.previousElementSibling.classList.add("lesson");
+                    x.innerText = "Виж повече";
+                }
+            })
+            btn.previousElementSibling.classList.remove("lesson");
+            btn.innerText = "Виж по-малко";
+        } else if (btn.innerText === "Виж по-малко") {
+            btn.previousElementSibling.classList.add("lesson");
+            btn.innerText = "Виж повече";
+        }
+    })
 });
