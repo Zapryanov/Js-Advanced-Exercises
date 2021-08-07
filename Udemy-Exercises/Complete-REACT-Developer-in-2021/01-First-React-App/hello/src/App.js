ccc46e354cb64fa9a50e6c2b24376699d1daad26
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-08-07 11:22:21 +0300

    feat: testing state

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/01-First-React-App/hello/src/App.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/01-First-React-App/hello/src/App.js
@@ -3,20 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      stringToShow: 'Hello! This is Text from the State!'
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello! This is test!</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>{this.state.stringToShow}</p>
+          <button onClick={() => this.setState({stringToShow: "The text has been changed!!!"})} >Change Text</button>
         </header>
       </div>
     )
