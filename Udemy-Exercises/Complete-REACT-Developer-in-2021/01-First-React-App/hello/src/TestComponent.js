dab69c54229b407940148951b008aeebb51b54fb
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-08-13 22:37:07 +0300

    feat: just for exercise

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/01-First-React-App/hello/src/TestComponent.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/01-First-React-App/hello/src/TestComponent.js
@@ -2,13 +2,16 @@ import React from 'react';
 import TheInnerComponent from './TheInnerComponent';
 
 const myName = "Ivan";
+const age = 41;
+const address = "Plovdiv";
 
 const TestComponent = (props) => {
+    console.log(props);
     
     return (
         <div>
             Hello from TestComponent - {props.testValue}...!
-            <TheInnerComponent name={myName}/>
+            <TheInnerComponent name={myName} age={age} address={address}/>
         </div>
     )
 }
--- Udemy-Exercises/Complete-REACT-Developer-in-2021/01-First-React-App/hello/src/TheInnerComponent.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/01-First-React-App/hello/src/TheInnerComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TheInnerComponent = (props) => {
     console.log(props)
     return (
-        <div>This is The Most Inner Component...!!! - Made from - {props.name}</div>
+        <div>This is The Most Inner Component...!!! - Made from - {props.name} ({props.age} years old from {props.address})</div>
     )
 }
 
