f0141e8f12cea79ca176893d721553785f8d7534
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-08-24 14:19:26 +0300

    feat: header module added

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/App.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import styles from './app.module.css';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className={styles.app}>
+      <Header />
     </div>
   );
 }
---
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const Header = () => {
+    return (
+        <header className={styles.navigation}>
+            <ul>
+                <li>Going to 1</li>
+                <li>Going to 2</li>
+                <li>Going to 3</li>
+                <li>Going to 4</li>
+                <li>Going to 5</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
