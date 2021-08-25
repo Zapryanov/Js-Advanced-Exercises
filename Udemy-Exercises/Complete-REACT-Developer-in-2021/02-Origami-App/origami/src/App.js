b13f35fc8bafb64d9dd46f7ecd3bcd5250a5f0e4
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-08-25 12:48:26 +0300

    feat: Aside component is added

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/App.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/App.js
@@ -1,10 +1,14 @@
 import styles from './app.module.css';
+import Aside from './components/aside';
 import Header from './components/header';
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <div>
+        <Aside />
+      </div>
     </div>
   );
 }
---
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/components/aside/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from '../link';
+
+const Aside = () => {
+    return (
+        <aside>
+            <Link href="#" title="Going to 1"/>                
+            <Link href="#" title="Going to 2"/>                
+            <Link href="#" title="Going to 3"/>                
+            <Link href="#" title="Going to 4"/>                
+            <Link href="#" title="Going to 5"/>                
+            <Link href="#" title="Going to 6"/>    
+        </aside>
+    )
+}
+
+export default Aside;
