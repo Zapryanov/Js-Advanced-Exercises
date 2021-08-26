b400f753b635d63135137286b9b53cc13c7aa1b6
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-08-26 10:25:59 +0300

    feat: CSS styles have been added to the Aside component

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/components/aside/index.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/components/aside/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Link from '../link';
+import styles from './index.module.css';
 
 const Aside = () => {
     return (
-        <aside>
+        <aside className={styles.container}>
             <Link href="#" title="Going to 1" type="aside"/>                
             <Link href="#" title="Going to 2" type="aside"/>                
             <Link href="#" title="Going to 3" type="aside"/>                
