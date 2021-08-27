9932e246b114b1ac88d934dba6733fbaeaee73d2
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-08-27 19:11:22 +0300

    feat: An Origami request has been made and submitted to the browser

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/components/origamis/index.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/components/origamis/index.js
@@ -2,12 +2,43 @@ import React, { Component } from 'react';
 import styles from './index.module.css';
 
 class Origamis extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            origamis: []
+        }
+    }
+
+    getOrigamis = async () => {
+        const promise = await fetch("http://localhost:9999/api/origami");
+        const origamis = await promise.json();
+
+        this.setState({
+            origamis
+        })
+    }
+
+    componentDidMount() {
+        this.getOrigamis();
+    }
 
     render() {
+        console.log(this.state.origamis);
+        const { origamis } = this.state;
+
         return (
             <div className={styles.container}>
                 <h1 className={styles.title}>Origamis</h1>
-                <div></div>
+                <div>
+                    {origamis.map(origam => {
+                        return(
+                            <div key={origam._id}>
+                                {origam.description}
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
