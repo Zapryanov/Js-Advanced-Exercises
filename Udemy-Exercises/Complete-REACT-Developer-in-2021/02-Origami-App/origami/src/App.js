3c5670c463f55c1fbd1b85e70b7ff2801bdea138
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-09-01 11:06:25 +0300

    feat: footer has been added

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/App.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/App.js
@@ -1,5 +1,6 @@
 import styles from './app.module.css';
 import Aside from './components/aside';
+import Footer from './components/footer';
 import Header from './components/header';
 import Origamis from './components/origamis';
 
@@ -11,6 +12,7 @@ function App() {
         <Aside />
         <Origamis />
       </div>
+      <Footer />
     </div>
   );
 }
