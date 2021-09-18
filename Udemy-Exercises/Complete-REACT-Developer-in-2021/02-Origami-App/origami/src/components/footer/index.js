288a13cb24863b38b464d48f748eb2837e44b61e
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-09-18 22:51:42 +0300

    feat: changes in footer

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/components/footer/index.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/components/footer/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Link from '../link';
 import styles from './index.module.css';
+import getNavigation from '../../utils/navigation';
 
 const Footer = () => {
+    const links = getNavigation();
     return (
         <footer className={styles.footer}>
             <div>
-                <Link href="#" title="Publications" type="footer"/>                
-                <Link href="#" title="Share your thoughts" type="footer"/>                
-                <Link href="#" title="Profile" type="footer"/>                
-                <Link href="#" title="Login" type="footer"/>                
-                <Link href="#" title="Register" type="footer"/>   
+                {
+                    links.map(navElement => {
+                        return (
+                            <Link href={navElement.link} title={navElement.title} type="footer"/>
+                        )
+                    })
+                }  
             </div>
             <p className={styles.university}>Software University ® 2021</p>
         </footer>
