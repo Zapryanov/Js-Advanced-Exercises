1b0ce0a174c84002c885855e9199beb77712098e
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-09-19 17:16:11 +0300

    feat: some changes

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/home-page/index.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/home-page/index.js
@@ -1,14 +1,54 @@
-import React from 'react';
-// import styles from './index.module.css';
-import Origamis from '../../components/origamis';
+import React, { Component } from 'react';
 import PageWrapper from '../../components/page-wrapper';
+import styles from './index.module.css';
+import Origam from '../../components/origam';
 
-function HomePage() {
-  return (
-    <PageWrapper>
-      <Origamis />
-    </PageWrapper>
-  );
+class HomePage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            origamis: []
+        }
+    }
+
+    getOrigamis = async () => {
+        const promise = await fetch("http://localhost:9999/api/origami");
+        const origamis = await promise.json();
+
+        this.setState({
+            origamis
+        })
+    }
+
+    renderOrigamis() {
+        const { origamis } = this.state;
+
+        return origamis.map((origam, i) => {
+            return(
+                <Origam  key={origam._id} index={i} {...origam}/>
+            )
+        })
+    }
+
+    componentDidMount() {
+        this.getOrigamis();
+    }
+
+    render() {
+        console.log(this.state.origamis);
+
+        return (
+          <PageWrapper>
+            <div className={styles.container}>
+                <h1 className={styles.title}>Publications</h1>
+                <div className={styles["origamis-wrapper"]}>
+                    {this.renderOrigamis()}
+                </div>
+            </div>
+          </PageWrapper>
+        )
+    }
 }
 
 export default HomePage;
