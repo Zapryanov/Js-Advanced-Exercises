444185c01c68135c496e26bc769fd1e4cda1fcc6
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-09-19 07:56:51 +0300

    feat: a pages folder has been created and all the pages will be there

---
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/home-page/home-page.js
@@ -0,0 +1,20 @@
+import styles from './home-page.module.css';
+import Aside from '../../components/aside';
+import Footer from '../../components/footer';
+import Header from '../../components/header';
+import Origamis from '../../components/origamis';
+
+function HomePage() {
+  return (
+    <div className={styles.app}>
+      <Header />
+      <div className={styles.container}>
+        <Aside />
+        <Origamis />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
