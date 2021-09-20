bb02daf38f65f736eac33a2051b5bbf5203239cc
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-09-20 17:42:19 +0300

    feat: new component

---
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/components/button/submit-button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './submit-buttons'
+
+const SubmitButton = ({ title }) => {
+    return (
+        <button className={styles.submit}></button>
+    )
+}
+
+export default SubmitButton;
