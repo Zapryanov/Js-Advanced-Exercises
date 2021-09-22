ff396772ef35d174435ec92b1439c67b2b829408
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-09-22 15:10:31 +0300

    feat: the layout has been added to login and register pages

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/LoginPage/index.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/LoginPage/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import SubmitButton from '../../components/button/submit-button';
+import PageLayout from '../../components/page-layout';
 import Title from '../../components/title';
 import style from './index.module.css';
 
 const LoginPage = () => {
     return (
-        <div className={style.container}>
-            <Title title="Login" />
+        <PageLayout>
+            <div className={style.container}>
+                <Title title="Login" />
 
-            <SubmitButton title="Login" />
-        </div>
+                <SubmitButton title="Login" />
+            </div>
+        </PageLayout>
     )
 }
 
--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/register-page/index.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/register-page/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import SubmitButton from '../../components/button/submit-button';
+import PageLayout from '../../components/page-layout';
 import Title from '../../components/title';
 import style from './index.module.css';
 
 const RegisterPage = () => {
     return (
-        <div className={style.container}>
-            <Title title="Register" />
+        <PageLayout>
+            <div className={style.container}>
+                <Title title="Register" />
 
-            <SubmitButton title="Register" />
-        </div>
+                <SubmitButton title="Register" />
+            </div>
+        </PageLayout>
     )
 }
 
