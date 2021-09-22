f3fde09bdf1b14970c1afe937f6ec653a3b427b5
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-09-22 15:00:04 +0300

    feat: login-path has been added to navigation

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/navigation.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/navigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/home-page';
+import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/register-page';
 import ShareThoughtsPage from './pages/share-thoughts';
 
@@ -12,6 +13,7 @@ const Navigation = () => {
                 <Route exact path="/" component={HomePage} />
                 <Route path="/share" component={ShareThoughtsPage} />
                 <Route path="/register" component={RegisterPage} />
+                <Route path="/login" component={LoginPage} />
             </Switch>
         </BrowserRouter>
     )
