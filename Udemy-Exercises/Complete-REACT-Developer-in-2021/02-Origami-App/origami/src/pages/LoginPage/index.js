675e0716ea2f3eac5833ba90942abc0fa9984cf9
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-09-22 23:28:20 +0300

    feat: input Component is created and Login and Register are almost completed

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/LoginPage/index.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/LoginPage/index.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SubmitButton from '../../components/button/submit-button';
+import Input from '../../components/input';
 import PageLayout from '../../components/page-layout';
 import Title from '../../components/title';
 import style from './index.module.css';
 
-const LoginPage = () => {
-    return (
-        <PageLayout>
-            <div className={style.container}>
-                <Title title="Login" />
+class LoginPage extends Component {
+    constructor(props) {
+        super(props)
 
-                <SubmitButton title="Login" />
-            </div>
-        </PageLayout>
-    )
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    onChange = (e, type) => {
+        const newState = {};
+        newState[type] = e.target.value;
+        this.setState(newState)
+    }
+
+    render() {
+        const {email, password } = this.state;
+
+        return (
+            <PageLayout>
+                <div className={style.container}>
+                    <Title title="Login" />
+                    <Input value={email} onChange={(ev) => this.onChange(ev, "email")} label="Email" id="email" />
+                    <Input value={password} onChange={(ev) => this.onChange(ev, "password")} label="Password" id="password" />
+                    <SubmitButton title="Login" />
+                </div>
+            </PageLayout>
+        )
+    }
 }
 
 export default LoginPage;
--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/register-page/index.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/register-page/index.js
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SubmitButton from '../../components/button/submit-button';
+import Input from '../../components/input';
 import PageLayout from '../../components/page-layout';
 import Title from '../../components/title';
 import style from './index.module.css';
 
-const RegisterPage = () => {
-    return (
-        <PageLayout>
-            <div className={style.container}>
-                <Title title="Register" />
+class RegisterPage extends Component {
+    constructor(props) {
+        super(props)
 
-                <SubmitButton title="Register" />
-            </div>
-        </PageLayout>
-    )
+        this.state = {
+            email: "",
+            password: "",
+            rePassword: ""
+        }
+    }
+
+    onChange = (e, type) => {
+        const newState = {};
+        newState[type] = e.target.value;
+        this.setState(newState);
+    }
+
+    render() {
+        const { email, password, rePassword } = this.state;
+
+        return (
+            <PageLayout>
+                <div className={style.container}>
+                    <Title title="Register" />
+                    <Input value={email} onChange={(e) => this.onChange(e, "email")} label="Email" id="email" />
+                    <Input value={password} onChange={(e) => this.onChange(e, "password")} label="Password" id="password" />
+                    <Input value={rePassword} onChange={(e) => this.onChange(e, "rePassword")} label="Re-Password" id="re-password" />
+                    <SubmitButton title="Register" />
+                </div>
+            </PageLayout>
+        )
+    }
 }
 
 export default RegisterPage;
