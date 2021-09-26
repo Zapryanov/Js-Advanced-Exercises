1478f8907b0d85b5bd4a4a4d68a84df7c2fe64c3
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-09-26 07:52:06 +0300

    feat: some new features

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/profile/index.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/profile/index.js
@@ -3,16 +3,51 @@ import Origamis from '../../components/origamis';
 import PageLayout from '../../components/page-layout';
 
 class ProfilePage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: null,
+            posts: null
+        }
+    }
 
     componentDidMount() {
-        
+        console.log(this.props.match.params.userId)
+        this.getUser(this.props.match.params.userId)
+    }
+
+    getUser = async(id) => {
+        const response = await fetch(`http://localhost:9999/api/user?id=${id}`);
+
+        if (!response.ok) {
+            this.props.history.push("/error")
+        }
+
+        const user = await response.json();
+
+        this.setState({
+            username: user.username,
+            posts: user.posts && user.posts.length,
+        })
     }
 
     render() {
+        const { username, posts } = this.state;
+
+        if (!username) {
+            return (
+                <PageLayout>
+                    <div>Loading.....</div>
+                </PageLayout>
+            )
+        }
+
         return (
             <PageLayout>
                 <div>
-
+            <p>Username: {username}</p>
+            <p>Posts: {posts}</p>
                 </div>
                 <Origamis length={3}/>
             </PageLayout>
