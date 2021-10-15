ab579f6382ecb6d874bbbbae63c9f3705f3f4e68
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-10-15 08:47:47 +0300

    feat: converting HompePage into a functional component

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/home-page/index.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/02-Origami-App/origami/src/pages/home-page/index.js
@@ -1,22 +1,35 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import PageLayout from '../../components/page-layout';
 // import styles from './index.module.css';
 import Title from '../../components/title';
 import Origamis from '../../components/origamis';
 import UserContext from '../../Context';
 
-class HomePage extends Component {
-    static contextType = UserContext;
 
-    render() {
-        console.log(this.context)
-        return (
-            <PageLayout>
-                <Title title="Publications" />
-                <Origamis />
-            </PageLayout>
-        )
-    }
+const HomePage = () => {
+    
+    const context = useContext(UserContext);
+    console.log(context)
+    return (
+        <PageLayout>
+            <Title title="Publications" />
+            <Origamis />
+        </PageLayout>
+    )
 }
+
+// class HomePage extends Component {
+//     static contextType = UserContext;
+
+//     render() {
+//         console.log(this.context)
+//         return (
+//             <PageLayout>
+//                 <Title title="Publications" />
+//                 <Origamis />
+//             </PageLayout>
+//         )
+//     }
+// }
 
 export default HomePage;
