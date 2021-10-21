47865a0212a4fecd416d9518fa61bf2a4afdb648
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-10-21 14:51:57 +0300

    feat: card-list-component is added

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/monsters-rolodex/src/App.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/monsters-rolodex/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list.component';
 
 class App extends Component {
   constructor() {
@@ -19,6 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <CardList name="TestName">Here showing the children of card-list.component</CardList>
         {
           this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
         }
