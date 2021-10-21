e36f9cd59bd0599df400becdecae2b3f55713c40
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-10-21 15:13:45 +0300

    feat: both files are moved to a separate folder

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/monsters-rolodex/src/App.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/monsters-rolodex/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { CardList } from './components/card-list.component';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
