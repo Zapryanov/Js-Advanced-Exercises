aa6d6e6233cbde423a2c51da135998d5f279fc39
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-10-22 21:37:12 +0300

    feat: feat: search bod component is ready

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/monsters-rolodex/src/App.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -30,7 +31,7 @@ class App extends Component {
     console.log(filteredMonsters)
     return (
       <div className="App">
-        <input type="search" placeholder="search monsters" onChange={(e) => this.handleSearchField(e)} />
+        <SearchBox placeholder="search monsters" handleChange={this.handleSearchField}/>
         <CardList monsters={filteredMonsters} />
       </div>
     );
