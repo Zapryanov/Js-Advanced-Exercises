acdf668e2317d0fdfe27ebd4f0a87433066a0c03
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-10-31 07:02:31 +0200

    feat: a new page has been added which will now be worked on

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/04-crwn-clothing/src/App.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/04-crwn-clothing/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import HomePage from './pages/home-page/homePage.component';
 import ShopPage from './pages/shop-page/shop.component';
 
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
