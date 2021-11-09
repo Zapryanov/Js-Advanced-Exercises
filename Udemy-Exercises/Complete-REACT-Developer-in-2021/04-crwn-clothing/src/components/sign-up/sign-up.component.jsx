234bb95e78e43dbb94f8692e6561e6e01bbcaa19
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-11-09 03:32:18 +0200

    feat: google authentication is complete and the creation of a registration component begins

---
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/04-crwn-clothing/src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default SignUp;
