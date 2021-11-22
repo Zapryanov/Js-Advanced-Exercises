9087100cc04abc2c95643202d42e843131336b4c
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-11-22 11:30:00 +0200

    feat: start work on a new checkout page component

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/04-crwn-clothing/src/App.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/04-crwn-clothing/src/App.js
@@ -4,10 +4,13 @@ import { connect  } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import './App.css';
+
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import HomePage from './pages/home-page/homePage.component';
 import ShopPage from './pages/shop-page/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.action';
 import { selectCurrentUser } from "./redux/user/user.selectors";
@@ -47,6 +50,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route 
             exact 
             path="/signin" 
