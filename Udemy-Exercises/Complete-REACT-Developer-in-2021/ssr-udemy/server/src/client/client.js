af60e35f709089c14caf29538809c981ef2563bc
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-12-22 10:37:38 +0200

    feat: reducers has been added into client.js file

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/ssr-udemy/server/src/client/client.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/ssr-udemy/server/src/client/client.js
@@ -6,8 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
-
 import Routes from "./Routes";
+import reducers from "./reducers";
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 
