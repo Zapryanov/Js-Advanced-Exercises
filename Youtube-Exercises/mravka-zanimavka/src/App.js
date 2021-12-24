f6a6d788973db2bf90fe86ac4f1cf2e7e1e9e028
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-12-24 13:56:36 +0200

    feat: react-router-dom has been installed

--- Youtube-Exercises/mravka-zanimavka/src/App.js
+++ Youtube-Exercises/mravka-zanimavka/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = useState(0);
 
   const increment = () => {
     setCount(count + 1);
