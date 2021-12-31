33f25a32c3dc4330907348a31f8b4cdd7c705c11
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2021-12-31 07:28:26 +0200

    feat: dummy data is added for testing

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/pages/HomePage.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/pages/HomePage.js
@@ -1,28 +1,33 @@
 const DUMMY_DATA = [
     {
         id: "m1",
-        title: "This is a first meetup",
+        title: "Демо събитие в Цар-Симеоновата градина - 1",
         image:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
         address: "Meetupstreet 5, 12345 Meetup City",
         description:
-        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+        "This is a FIRST, amazing meetup which you definitely should not miss. It will be a lot of fun!",
     },
     {
         id: "m2",
-        title: "This is a second meetup",
+        title: "Демо събитие в Цар-Симеоновата градина - 2",
         image:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
         address: "Meetupstreet 5, 12345 Meetup City",
         description:
-        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+        "This is a SECOND, amazing meetup which you definitely should not miss. It will be a lot of fun!",
     },
 ];
 
 function HomePage() {
     return (
         <div>
-        <h1>Мравка Занимавка</h1>
+            <h1>Мравка Занимавка</h1>
+            <section>
+                {DUMMY_DATA.map(data => {
+                    return ( <li key={data.id}>{data.title}</li>)
+                })}
+            </section>
         </div>
     );
 }
