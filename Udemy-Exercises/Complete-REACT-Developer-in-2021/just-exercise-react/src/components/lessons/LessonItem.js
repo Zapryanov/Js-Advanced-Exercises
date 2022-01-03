c0ecf0b8934e00072d73f3b3239e9797987420d0
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-03 09:25:31 +0200

    feat: Card component using props.children

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/components/lessons/LessonItem.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/components/lessons/LessonItem.js
@@ -1,14 +1,17 @@
+import Card from '../ui/Card';
 import styles from './LessonItem.module.css';
 
 function LessonItem(props) {
     return (
         <li className={styles["current-article"]}>
-            <h4 className={`${styles["test-1"]} ${styles["test-2"]}`}>{props.title}</h4>
-            <div className={styles["wrap-img-article"]}>
-                <img src={props.image} alt={props.title} />
-            </div>
-            <p className={`${styles["wrap-text-article"]} ${styles["lesson"]}`}>{props.description}</p>
-            <button className={`${styles["btn-show-text"]} ${styles["lesson-page"]}`}>Покажи повече</button>
+            <Card>
+                <h4 className={`${styles["test-1"]} ${styles["test-2"]}`}>{props.title}</h4>
+                <div className={styles["wrap-img-article"]}>
+                    <img src={props.image} alt={props.title} />
+                </div>
+                <p className={`${styles["wrap-text-article"]} ${styles["lesson"]}`}>{props.description}</p>
+                <button className={`${styles["btn-show-text"]} ${styles["lesson-page"]}`}>Покажи повече</button>
+            </Card>
         </li>
     )
 }
