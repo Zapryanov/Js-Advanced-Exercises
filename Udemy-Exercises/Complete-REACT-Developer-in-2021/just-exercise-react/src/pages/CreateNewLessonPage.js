469aab4405d59b9cca2ddfebbe12d0a96726c83c
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-05 14:16:56 +0200

    feat: use a history object to navigate to the appropriate page

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/pages/CreateNewLessonPage.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/pages/CreateNewLessonPage.js
@@ -1,6 +1,10 @@
+import { useHistory } from 'react-router-dom'
+
 import NewLessonForm from "../components/lessons/NewLessonForm";
 
 function CreateNewLessonPage() {
+    const history = useHistory();
+
     function addLessonHandler(lessonData) {
         fetch(
             'https://mravka-zanimavka-default-rtdb.europe-west1.firebasedatabase.app/lessons.json',
@@ -11,7 +15,9 @@ function CreateNewLessonPage() {
                     'Content-Type': 'application/json'
                 }
             }
-        )
+        ).then(() => {
+            history.replace('/lessons')
+        })
     }
 
     return (
