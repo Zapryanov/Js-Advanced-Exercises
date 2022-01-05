6814c97420ea25c0ac985dfd1390de7cc2911aaa
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-05 11:41:32 +0200

    feat: added functionality to the NewLessonForm.js

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/components/lessons/NewLessonForm.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/components/lessons/NewLessonForm.js
@@ -1,21 +1,45 @@
+import { useRef } from 'react';
+
 import Card from '../ui/Card';
 import styles from './NewLessonForm.module.css';
 
 function NewLessonForm() {
+    const titleInputRef = useRef();
+
+    const imageInputRef = useRef();
+
+    const textInputRef = useRef();
+
+    function submitHandler(e) {
+        e.preventDefault();
+
+        const title = titleInputRef.current.value;
+        const image = imageInputRef.current.value;
+        const text = textInputRef.current.value;
+
+        const lessonData = {
+            title,
+            image,
+            text
+        }
+        
+        console.log(lessonData)
+    }
+
     return (
         <Card>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={submitHandler}>
                 <div className={styles.control}>
                     <label htmlFor='title'>Заглавие на урока</label>
-                    <input type="text" required id='title' />
+                    <input type="text" required id='title' ref={titleInputRef} />
                 </div>
                 <div className={styles.control}>
                     <label htmlFor='image'>Снимка на урока</label>
-                    <input type="text" required id='image' />
+                    <input type="text" required id='image' ref={imageInputRef} />
                 </div>
                 <div className={styles.control}>
                     <label htmlFor='description'>Добавяне на текст</label>
-                    <textarea id='description' required rows='5'></textarea>
+                    <textarea id='description' required rows='5' ref={textInputRef}></textarea>
                 </div>
                 <div className={styles.actions}>
                     <button>Добави урок</button>
