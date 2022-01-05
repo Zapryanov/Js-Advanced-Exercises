0dd06224aedef263e82af0066ecb5d74de34faec
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-05 14:03:15 +0200

    feat: creating a POST request

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/pages/CreateNewLessonPage.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/pages/CreateNewLessonPage.js
@@ -1,10 +1,23 @@
 import NewLessonForm from "../components/lessons/NewLessonForm";
 
 function CreateNewLessonPage() {
+    function addLessonHandler(lessonData) {
+        fetch(
+            'https://mravka-zanimavka-default-rtdb.europe-west1.firebasedatabase.app/lessons.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(lessonData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+    }
+
     return (
         <section>
             <h1>Добави нов урок</h1>
-            <NewLessonForm />
+            <NewLessonForm onAddLesson={addLessonHandler} />
         </section>
     )
 }
