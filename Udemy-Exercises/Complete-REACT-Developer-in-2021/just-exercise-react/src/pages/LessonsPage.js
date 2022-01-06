4749706093f0767d536be4f5bcaa7d6373ba50b0
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-06 05:32:22 +0200

    feat: application of useEffect and useState hooks and fetching data

--- Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/pages/LessonsPage.js
+++ Udemy-Exercises/Complete-REACT-Developer-in-2021/just-exercise-react/src/pages/LessonsPage.js
@@ -1,31 +1,46 @@
+import { useState, useEffect } from "react";
+
 import LessonList from "../components/lessons/LessonList";
 
-const DUMMY_DATA = [
-    {
-        id: "m1",
-        title: "Демо събитие в Цар-Симеоновата градина - 1",
-        image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-        address: "Meetupstreet 5, 12345 Meetup City",
-        description:
-        "This is a FIRST, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-    },
-    {
-        id: "m2",
-        title: "Демо събитие в Цар-Симеоновата градина - 2",
-        image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-        address: "Meetupstreet 5, 12345 Meetup City",
-        description:
-        "This is a SECOND, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-    },
-];
+//  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
 
 function LessonsPage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedLessons, setLoadedLessons] = useState([]);
+    
+    useEffect(() => {
+        setIsLoading(true);
+
+        fetch('https://mravka-zanimavka-default-rtdb.europe-west1.firebasedatabase.app/lessons.json')
+            .then(response => {
+                return response.json()
+            }).then(data => {
+                const lessons = [];
+
+                for (let key in data) {
+                    const lesson = {
+                        id: key,
+                        ...data[key]
+                    };
+                    lessons.push(lesson)
+                }
+                setIsLoading(false);
+                setLoadedLessons(lessons);
+            })
+    }, []);
+
+    if (isLoading) {
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+        )
+    }
+
     return (
         <div className="wrap-all-lessons">
             <h1>Уроци</h1>
-            <LessonList lessons={DUMMY_DATA} />
+            <LessonList lessons={loadedLessons} />
         </div>
     )
 }
