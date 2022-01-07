1240c695e848b41a61b1056a28d823de8bf47821
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-07 15:17:06 +0200

    feat: a little exercise with routing

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/clients/[id]/[clientProjectId].js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/clients/[id]/[clientProjectId].js
@@ -3,10 +3,15 @@ import { useRouter } from 'next/router';
 function SelectedClientProjectPage() {
     const router = useRouter();
 
+    function returnToTheClientsPage() {
+        router.push(`/clients/${router.query.id}`)
+    }
+
     console.log(router.query);
     return (
         <div>
             <h1>The Project Page for a Specific Project for a Selected Client</h1>
+            <button onClick={returnToTheClientsPage}>Go to The Projects of a given Client</button>
         </div>
     )
 }
--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/clients/[id]/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/clients/[id]/index.js
@@ -6,14 +6,20 @@ function ClientProjectsPage() {
 
     console.log(router.query);
 
+    function loadProjectHandler() {
+        console.log("Yes...");
+        router.push(`/clients/${router.query.id}/projectA`)
+    }
+
     return (
         <div>
             <h1>The projects of a given Client</h1>
             <ul>
                 <li>
-                    <Link href="../clients">Go back to the Clients Page</Link>
+                    <Link href="/clients">Go back to the Clients Page</Link>
                 </li>
             </ul>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     )
 }
