4dc6a228bf7a85c516da622e074d7db884d7b4a7
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-10 14:58:13 +0200

    feat: POST request is ready

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/addNewLesson/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/addNewLesson/index.js
@@ -1,10 +1,23 @@
 import LessonForm from "../../components/LessonForm";
 
 function AddNewLesson(props) {
+    function addLessonHandler(lessonData) {
+        fetch(
+            "https://mravka-zanimavka-default-rtdb.europe-west1.firebasedatabase.app/lessons.json",
+            {
+                method: "POST",
+                body: JSON.stringify(lessonData),
+                headers: {
+                    "Content-Type": "application/json"
+                } 
+            }
+        )
+    }
+
     return (
         <section>
             <h1>Добави нов урок</h1>
-            <LessonForm onAddLesson={}/>
+            <LessonForm onAddLesson={addLessonHandler}/>
         </section>
     )
 }
