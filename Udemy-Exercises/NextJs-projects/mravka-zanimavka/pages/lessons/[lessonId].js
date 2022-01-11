ce97f371e1e4505c6e4528b6b398cf60a2632b1d
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-11 13:39:00 +0200

    feat: visualize an element

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/lessons/[lessonId].js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/lessons/[lessonId].js
@@ -1,9 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
 import { useRouter } from 'next/router';
+import LessonList from '../../components/lessons/LessonList';
 
 function CurrentLessonId() {
+    // const [ isLoading, setIsLoading ] = useState(true);
+    const [ loadedLesson, setLoadedLesson ] = useState([]);
+
     const router = useRouter();
 
-    console.log("router query - ", router.query);
+    console.log("router query",router.query.lessonId);
+    const lessonId = router.query.lessonId;
+
+    useEffect(() => {
+        fetch(`https://mravka-zanimavka-default-rtdb.europe-west1.firebasedatabase.app/lessons.json`)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            console.log(data)
+
+            const theLesson = [];
+
+            for (let key in data) {
+                if (key === lessonId) {
+                    const lesson = {
+                        id: key,
+                        ...data[key]
+                    }
+                    theLesson.push(lesson)
+                }
+            }
+            setLoadedLesson(theLesson)
+        })
+    }, [lessonId]);
 
     // Send request to some backend server to fetch 
     // the piece of data with an ID of router.query.lessonId
@@ -11,7 +41,8 @@ function CurrentLessonId() {
     return (
         <div>
             <h1>Current Lesson Id</h1>
-            <h2>{router.query.lessonId}</h2>
+            <h2>{lessonId}</h2>
+            <LessonList lessons={loadedLesson} />
         </div>
     )
 }
