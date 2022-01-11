ea2d49a8a0e30ebd7ee38cda272939231052f64f
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-11 10:42:17 +0200

    feat: unnecessary comments have been removed

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/lessons/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/lessons/index.js
@@ -2,27 +2,6 @@ import React, { useEffect, useState } from "react";
 
 import LessonList from "../../components/lessons/LessonList";
 
-// const DUMMY_DATA = [
-//     {
-//       id: 'm1',
-//       title: 'This is a first meetup',
-//       image:
-//         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-//       text: 'Meetupstreet 5, 12345 Meetup City',
-//       description:
-//         'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-//     },
-//     {
-//       id: 'm2',
-//       title: 'This is a second meetup',
-//       image:
-//         'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-//       text: 'Meetupstreet 5, 12345 Meetup City',
-//       description:
-//         'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-//     },
-// ];
-
 function LessonsPage() {
     const [ isLoading, setIsLoading ] = useState(true);
     const [ loadedLessons, setLoadedLessons ] = useState([]);
@@ -62,7 +41,6 @@ function LessonsPage() {
         <div className="wrap-all-lessons">
             <h1>Lessons Page</h1>
             <div>
-                {/* <LessonList lessons={DUMMY_DATA} /> */}
                 <LessonList lessons={loadedLessons} />
             </div>
         </div>
