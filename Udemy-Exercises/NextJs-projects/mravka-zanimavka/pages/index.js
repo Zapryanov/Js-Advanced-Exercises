15402820208f2a40d97445d1ef97e2608fd6bed2
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-12 10:57:38 +0200

    feat: using function getStaticProps to test server side rendering by NextJs

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/index.js
@@ -1,16 +1,31 @@
 import Link from 'next/link';
 
-function HomePage() {
+function HomePage(props) {
+    const { lessons } = props;
+
     return (
         <div>
-            <h1>The Home Page</h1>
+            {/* <h1>The Home Page</h1>
             <ul>
                 <li>
                     <Link href="/lessons">All lessons</Link>
                 </li>
+            </ul> */}
+            <ul>
+                {lessons.map(lesson => {
+                    <li key={lesson.id}>{lesson.title}</li>
+                })}
             </ul>
         </div>
     )
+}
+
+export async function getStaticProps() {
+    return { 
+        props : {
+            lessons: [{id: "id-1", title: "Lesson-1"}]
+        }
+    }
 }
 
 export default HomePage;
