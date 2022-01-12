5f2bbb213360f77a9eb607fea109eae5965b6295
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-12 22:40:28 +0200

    feat: new project just for exercise in NextJs

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/index.js
@@ -1,31 +1,16 @@
 import Link from 'next/link';
 
-function HomePage(props) {
-    const { lessons } = props;
-
+function HomePage() {
     return (
         <div>
-            {/* <h1>The Home Page</h1>
+            <h1>The Home Page</h1>
             <ul>
                 <li>
                     <Link href="/lessons">All lessons</Link>
                 </li>
-            </ul> */}
-            <ul>
-                {lessons.map(lesson => {
-                    <li key={lesson.id}>{lesson.title}</li>
-                })}
             </ul>
         </div>
     )
-}
-
-export async function getStaticProps() {
-    return { 
-        props : {
-            lessons: [{id: "id-1", title: "Lesson-1"}]
-        }
-    }
 }
 
 export default HomePage;
