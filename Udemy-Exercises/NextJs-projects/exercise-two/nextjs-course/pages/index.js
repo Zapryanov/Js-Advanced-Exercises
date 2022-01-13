7eb03ba636633dcede68807cc1f7a9a6cbe22515
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-13 11:12:00 +0200

    feat: dummy-data file have been moved into data folder and I used for testing the function getStaticProps

--- Udemy-Exercises/NextJs-projects/exercise-two/nextjs-course/pages/index.js
+++ Udemy-Exercises/NextJs-projects/exercise-two/nextjs-course/pages/index.js
@@ -1,7 +1,28 @@
-export default function Home() {
+function Home(props) {
+  const { products } = props;
+
   return (
     <div>
       <h1>Hello Next World</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+      <ul>
+        {products.map(product =><li key={product.id}>{product.title}</li>)}
+      </ul>
     </div>
   )
 }
+
+export async function getStaticProps() {
+  return {
+    props: 
+      {
+        products: [{ id: "p1", title: "Product-P1" }]
+      }
+  }
+}
+
+export default Home;
