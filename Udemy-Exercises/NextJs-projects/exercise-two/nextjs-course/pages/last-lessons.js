b131553f200b47d4c96dc4a980b687c4722254d3
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-18 08:37:53 +0200

    feat: combining Pre-Fetching with Client-Side-Fetching

--- Udemy-Exercises/NextJs-projects/exercise-two/nextjs-course/pages/last-lessons.js
+++ Udemy-Exercises/NextJs-projects/exercise-two/nextjs-course/pages/last-lessons.js
@@ -1,16 +1,14 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
-function LastSalesPage() {
-    const [lessons, setLessons] = useState();
-    const [isLoading, setIsLoading] = useState(false);
+function LastLessonsPage(props) {
+    const [lessons, setLessons] = useState(props.lessons);
+    // const [isLoading, setIsLoading] = useState(false);
+
+    const {data, error} = useSWR("https://mravka-zanimavka-default-rtdb.europe-west1.firebasedatabase.app/lessons.json");
 
     useEffect(() => {
-        setIsLoading(true);
-
-        fetch(
-            "https://mravka-zanimavka-default-rtdb.europe-west1.firebasedatabase.app/lessons.json"
-        ).then(response => response.json())
-        .then(data => {
+        if (data) {
             const transformedLessons = [];
 
             for(let key in data) {
@@ -23,27 +21,82 @@ function LastSalesPage() {
             }
 
             setLessons(transformedLessons);
-            setIsLoading(false);
-        })
-    }, []);
+        }
+    }, [data]);
 
-    if(isLoading) {
-        return (
-            <h1>Loading...</h1>
-        )
+    // --------------------------------------------------------------------------
+
+    // useEffect(() => {
+    //     setIsLoading(true);
+
+    //     fetch(
+    //         "https://mravka-zanimavka-default-rtdb.europe-west1.firebasedatabase.app/lessons.json"
+    //     ).then(response => response.json())
+    //     .then(data => {
+    //         const transformedLessons = [];
+
+    //         for(let key in data) {
+    //             transformedLessons.push({ 
+    //                 id: key, 
+    //                 image: data[key].image, 
+    //                 text: data[key].text, 
+    //                 title: data[key].title 
+    //             });
+    //         }
+
+    //         setLessons(transformedLessons);
+    //         setIsLoading(false);
+    //     })
+    // }, []);
+
+    // if(isLoading) {
+    //     return (
+    //         <h1>Loading...</h1>
+    //     )
+    // }
+
+    // if (!lessons) {
+    //     return (
+    //         <h1>No lessons yet!</h1>
+    //     )
+    // }
+
+    // ----------------------------------------------------------
+
+    if (error) {
+        return  <h1>Failed to load!!!</h1>
     }
 
-    if (!lessons) {
-        return (
-            <h1>No lessons yet!</h1>
-        )
+    if (!data && !lessons) {
+        return  <h1>Loading...</h1>
     }
 
     return (
         <ul>
-            {lessons.map(lesson => <li key={lesson.id}>{lesson.title}: {lesson.text}</li>)}
+            {lessons.map(lesson => (
+                <li key={lesson.id}>{lesson.title}: {lesson.text}</li>
+            ))}
         </ul>
     )
 }
 
-export default LastSalesPage;
+export async function getStaticProps() {
+    const response = await fetch(
+        "https://mravka-zanimavka-default-rtdb.europe-west1.firebasedatabase.app/lessons.json"
+    )
+    const data = await response.json();
+        const transformedLessons = [];
+
+        for(let key in data) {
+            transformedLessons.push({ 
+                id: key, 
+                image: data[key].image, 
+                text: data[key].text, 
+                title: data[key].title 
+            });
+        }
+
+        return { props: { lessons: transformedLessons } }
+}
+
+export default LastLessonsPage;
