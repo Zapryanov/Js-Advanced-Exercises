dc23299fabcb71de3b9ac1e4438e5514aaadd65c
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-23 09:20:07 +0200

    feat: I can't fix a small bug with Image

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/lessons/LessonItem.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/lessons/LessonItem.js
@@ -1,3 +1,5 @@
+// import Image from "next/image";
+
 import ButtonOne from "../ui/buttonOne";
 import styles from "./LessonItem.module.css";
 
@@ -10,6 +12,7 @@ function LessonItem(props) {
             <div className={styles["current-article"]}>
                 <h4>{title}</h4>
                 <div className={styles["wrap-img-article"]}>
+                    {/* <Image src={`/${image}`} alt={title} width={340} height={160} /> */}
                     <img src={image} alt={title} />
                 </div>
                 <p className={`${styles["wrap-text-article"]} ${styles.italic} ${styles.one}`}>{text}</p>
