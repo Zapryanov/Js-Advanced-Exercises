28afffd486cd639500447f76d55ea7d41d0e48ca
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-26 06:32:16 +0200

    feat: two functions were exported for use in multiple files and separate feedback folder was created

---
+++ Udemy-Exercises/NextJs-projects/api-routes/pages/feedback/index.js
@@ -0,0 +1,17 @@
+function FeedbackPage(props) {
+    return (
+        <ul>
+            {props.feedbackItems.map((item) => {
+                <li key={item.id}>{item.text}</li>
+            })}
+        </ul>
+    )
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+            
+        }
+    }
+}
