cdcc6dea96823a29df47ca6810e5728e9ff52f4b
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-01-31 19:00:49 +0200

    feat: using getStaticProps and getStaticPaths

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/lessons/[lessonId].js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/lessons/[lessonId].js
@@ -1,16 +1,62 @@
 import { useRouter } from "next/router";
 
-function CurrentLesson() {
+function CurrentLesson(props) {
     const router = useRouter();
 
-    console.log(router.pathname);
-    console.log(router.query);
+    function goBack() {
+        router.push("/lessons")
+    }
 
     return (
         <div>
-            <h1>Current Lesson</h1>
+            <h1>{props.title}</h1>
+            <div>
+                Image
+            </div>
+            <p>{props.text}</p>
+            <button onClick={goBack}>Go back</button>
         </div>
     )
+}
+
+export async function getStaticProps(context) {
+    const { params } = context;
+    console.log("From getStaticProps in lessonId - ", params);
+
+    const lessonId = params.lessonId;
+
+    // Fetch data from external API
+    const res = await fetch(`https://mravka-zanimavka-default-rtdb.europe-west1.firebasedatabase.app/lessons.json`)
+    const data = await res.json()
+  
+    // Transform the data from Firebase
+    const loadedLessons = [];
+    
+    for (const key in data) {
+        loadedLessons.push({
+            id: key, 
+            ...data[key]
+        })
+    }
+
+    const currentLesson = loadedLessons.find(lesson => lesson.id === lessonId);
+
+    return {
+        props: {
+            lesson: currentLesson
+        }
+    }
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [
+            { params: { lessonId: "-Msdiga337qQ9LruZGKQ" } },
+            { params: { lessonId: "-Mse90PRarcejkqEh-HC" } },
+            { params: { lessonId: "-Mt2PeaBhBjJr4XygjKg" } }
+        ],
+        fallback: true
+    }
 }
 
 export default CurrentLesson;
