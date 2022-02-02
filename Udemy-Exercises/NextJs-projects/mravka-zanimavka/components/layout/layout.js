55eaf6ec8fca49ba98ff7b2b67466674f8d3119e
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-02-02 17:28:20 +0200

    feat: Layout component is added

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/layout/layout.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/layout/layout.js
@@ -1,12 +1,13 @@
-import { Fragment } from "react";
 import Header from "./header";
+
+import styles from "./layout.module.css";
 
 function Layout(props) {
     return (
-        <Fragment>
+        <div className={styles.container}>
             <Header />
             <main>{props.children}</main>
-        </Fragment>
+        </div>
     )
 }
 
