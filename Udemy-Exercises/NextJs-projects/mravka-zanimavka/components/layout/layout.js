8d855fd08ef221efc86ac18446f7980f3035ed0c
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-02-02 16:44:26 +0200

    feat: header is added to layout

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/layout/layout.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/layout/layout.js
@@ -1,8 +1,10 @@
 import { Fragment } from "react";
+import Header from "./header";
 
 function Layout(props) {
     return (
         <Fragment>
+            <Header />
             <main>{props.children}</main>
         </Fragment>
     )
