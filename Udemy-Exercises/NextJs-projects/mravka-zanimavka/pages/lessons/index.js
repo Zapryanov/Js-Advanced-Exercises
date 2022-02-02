13f2900121de37750591a29c7d3d826eb109ecbb
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-02-02 16:23:15 +0200

    feat: using Head from next/head

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/lessons/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/lessons/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import Head from "next/head";
 
 import LessonList from "../../components/lessons/lesson-list";
 
@@ -6,6 +6,11 @@ function LessonsPage(props) {
     
     return (
         <div>
+            <Head>
+                <title>Уроци</title>
+                <meta name="description" content="уроци по китайски с мравка занимавка в град пловдив" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             <h1>Lessons Page</h1>
             <LessonList lessons={props.loadedLessons} />
         </div>
