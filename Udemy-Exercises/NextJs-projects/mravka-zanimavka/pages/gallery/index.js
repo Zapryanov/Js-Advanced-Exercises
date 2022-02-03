0d0cf81808c02a4e82837832aa631a57fd2497f3
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-02-03 18:44:18 +0200

    feat: the initial form of the gallery page has been added

---
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/gallery/index.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react/cjs/react.production.min";
+
+function GalleryPage() {
+    return (
+        <Fragment>
+            <h1>Gallery Page</h1>
+        </Fragment>
+    )
+}
+
+export default GalleryPage;
