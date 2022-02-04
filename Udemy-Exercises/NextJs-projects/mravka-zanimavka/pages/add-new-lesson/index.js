8e80ba4584528aaaedf408737e6a3e4c6de27516
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-02-04 20:49:17 +0200

    feat: a few additions to the add-new-lesson page

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/add-new-lesson/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/add-new-lesson/index.js
@@ -1,21 +1,41 @@
+import { useRef } from "react";
+
 function AddNewLesson() {
+    const titleInputRef = useRef();
+    const urlInputRef = useRef();
+    const textInputRef = useRef();
+    
+    function submitFormHandler(e) {
+        e.preventDefault();
+
+        const enteredTtitle = titleInputRef.current.value;
+        const enteredUrl = urlInputRef.current.value;
+        const enteredText = textInputRef.current.value;
+
+        console.log(enteredTtitle, enteredUrl, enteredText);
+
+        if (enteredTtitle !== "" && enteredUrl !== "" && enteredText !== "") {
+            
+        }
+    }
+
     return (
         <div>
             <h1>Добави нов Урок</h1>
             <form>
                 <div>
                     <label htmlFor="title">Добави заглавие: </label>
-                    <input type="text" id="title" />
+                    <input type="text" id="title" ref={titleInputRef} />
                 </div>
                 <div>
                     <label htmlFor="url">Добави URL на снимка: </label>
-                    <input type="url" id="url" />
+                    <input type="url" id="url" ref={urlInputRef} />
                 </div>
                 <div>
                     <label htmlFor="text">Добави текст: </label>
-                    <textarea id="text" rows="5"></textarea>
+                    <textarea id="text" rows="5" ref={textInputRef} ></textarea>
                 </div>
-                <button>Добави урока</button>
+                <button onClick={submitFormHandler}>Добави урока</button>
             </form>
         </div>
     )
