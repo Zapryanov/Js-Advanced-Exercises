5753e946b868a6a9384b38cb82f2dec0c1ec2393
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-02-12 16:54:04 +0200

    feat: css effect was added to the button

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/index.js
@@ -35,7 +35,7 @@ function HomePage() {
                 </div>
             </article>
             <article className={`${styles.description} ${styles.three}`}>
-                <div>
+                <div className={styles["leaf-effect"]}>
                     <a href="https://www.facebook.com/MravkaZanimavka">
                         <Image width={1200} height={200} src="https://res.cloudinary.com/audipower/image/upload/v1643362891/save-lesson_izgw4s.png" alt="link-facebook" />
                     </a>
