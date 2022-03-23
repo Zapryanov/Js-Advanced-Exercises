fe20ace60692e3c84c60e89e05590d36a3b31eaa
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-03-23 21:22:56 +0200

    feat: the head component from next/head was added in contacts page and two unnecessary files were removed

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/contacts/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/contacts/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./index.module.css";
+import Head from "next/head";
 
 import GoogleMapReact from 'google-map-react';
 import Link from "next/link";
@@ -18,54 +19,60 @@ function ContactsPage() {
     };
 
     return (
-        <div className={styles["wrap-contacts-page"]}>
-            <h1>Контакти</h1>
-            <section className={styles.contacts}>
-                <div className={styles.center}>
-                    <div className={styles["wrap-socials"]}>
-                        <span>
-                            <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1644830649/mail_qbj18y.svg" alt="email" />
-                        </span>
-                        <span>mravkazanimavka@gmail.com</span>
+        <>
+            <Head>
+                <title>Контакти</title>
+                <meta name="description" content="За контакти - град Пловдив, улица „Йоаким Груев“ 15 А. Обучение по нглийски и китайски език за деца" />
+            </Head>
+            <div className={styles["wrap-contacts-page"]}>
+                <h1>Контакти</h1>
+                <section className={styles.contacts}>
+                    <div className={styles.center}>
+                        <div className={styles["wrap-socials"]}>
+                            <span>
+                                <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1644830649/mail_qbj18y.svg" alt="email" />
+                            </span>
+                            <span>mravkazanimavka@gmail.com</span>
+                        </div>
+                        <div className={styles["wrap-socials"]}>
+                            <span>
+                                <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1644830666/facebook_gt0ii9.svg" alt="email" />
+                            </span>
+                            <span>Facebook:
+                                <Link href="https://www.facebook.com/MravkaZanimavka">
+                                    <a className={styles.linkFacebook}>
+                                        &nbsp;Мравка Занимавка
+                                    </a>
+                                </Link>
+                            </span>
+                        </div>
+                        <div className={styles["wrap-socials"]}>
+                            <span>
+                                <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1644830685/home-icon_jcbh4g.png" alt="email" />
+                            </span>
+                            <span>Пловдив, ул. &#8222;Йоаким Груев&#8220; 15 А</span>
+                        </div>
                     </div>
-                    <div className={styles["wrap-socials"]}>
-                        <span>
-                            <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1644830666/facebook_gt0ii9.svg" alt="email" />
-                        </span>
-                        <span>Facebook:
-                            <Link href="https://www.facebook.com/MravkaZanimavka">
-                                <a className={styles.linkFacebook}>
-                                    &nbsp;Мравка Занимавка
-                                </a>
-                            </Link>
-                        </span>
+                </section>
+                <section>
+                    <div className={`${styles["wrap-google-maps"]} ${styles.maps}`}>
+                        <GoogleMapReact
+                            bootstrapURLKeys={{ key: process.env.bootstrapURLKeys }}
+                            defaultCenter={{ lat: Number(process.env.lat), lng: Number(process.env.lng) }}
+                            defaultZoom={17}
+                            yesIWantToUseGoogleMapApiInternals
+                            onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
+                        >
+                            <AnyReactComponent
+                                lat={Number(process.env.lat)}
+                                lng={Number(process.env.lng)}
+                                text="Мравка Занимавка"
+                            />
+                        </GoogleMapReact>
                     </div>
-                    <div className={styles["wrap-socials"]}>
-                        <span>
-                            <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1644830685/home-icon_jcbh4g.png" alt="email" />
-                        </span>
-                        <span>Пловдив, ул. &#8222;Йоаким Груев&#8220; 15 А</span>
-                    </div>
-                </div>
-            </section>
-            <section>
-                <div className={`${styles["wrap-google-maps"]} ${styles.maps}`}>
-                    <GoogleMapReact
-                        bootstrapURLKeys={{ key: process.env.bootstrapURLKeys }}
-                        defaultCenter={{ lat: Number(process.env.lat), lng: Number(process.env.lng) }}
-                        defaultZoom={17}
-                        yesIWantToUseGoogleMapApiInternals
-                        onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
-                    >
-                        <AnyReactComponent
-                            lat={Number(process.env.lat)}
-                            lng={Number(process.env.lng)}
-                            text="Мравка Занимавка"
-                        />
-                    </GoogleMapReact>
-                </div>
-            </section>
-        </div>
+                </section>
+            </div>
+        </>
     )
 }
 
