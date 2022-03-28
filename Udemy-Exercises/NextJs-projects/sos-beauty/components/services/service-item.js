4ad55b7482d10e5faec0230c67430ad42d48766d
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-03-28 09:53:36 +0300

    feat: few styles have been added

--- Udemy-Exercises/NextJs-projects/sos-beauty/components/services/service-item.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/components/services/service-item.js
@@ -1,21 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import styles from "./service-item.module.css";
+
 function ServiceItem(props) {
     const { id, image, titleBig, titleSmall } = props;
 
     return (
-        <li key={id}>
+        <li key={id} className={styles["service-item"]}>
             <div>
-                <h2>{titleBig}</h2>
-                <h3>{titleSmall}</h3>
+                <div className={styles["wrap-main-name"]}>
+                    <h2>{titleBig}</h2>
+                </div>
+                <div className={styles["wrap-secondary-name"]}>
+                    <h3>{titleSmall}</h3>
+                </div>
             </div>
-            <div>
+            <div className={styles["wrap-image-service"]}>
                 <Image width={500} height={400} src={image} alt={titleBig} />
             </div>
             <div>
                 <Link href={`/cosmetics/${id}`}>
-                    <a>Виж повече</a>
+                    <a className={styles["btn-service"]}>Виж повече</a>
                 </Link>
             </div>
         </li>
