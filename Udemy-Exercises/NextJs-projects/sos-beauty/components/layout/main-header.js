fe88f9d69cf518f149ec16f518c4a238e0f8df2e
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-03-28 15:45:30 +0300

    feat: simple header added with head nextjs component

---
+++ Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/main-header.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+import styles from "./main-header.module.css";
+
+function MainHeader() {
+    return (
+        <header className={styles["main-header"]}>
+            <div>
+                <h1>S.O.S Beauty</h1>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="https://www.facebook.com/S.O.SBeauti/">Facebook</Link> 
+                    </li>
+                    <li>
+                        <Link href="https://www.facebook.com/S.O.SBeauti/">Instagram</Link> 
+                    </li>
+                    <li>
+                        <Link href="https://www.facebook.com/S.O.SBeauti/">Twitter</Link> 
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainHeader;
--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/index.js
@@ -1,9 +1,14 @@
-import ServicesList from "../components/services/services-list"
-import { getAllServices } from "../dummy-data"
+import ServicesList from "../components/services/services-list";
+import { getAllServices } from "../dummy-data";
+import Head from "next/head";
 
 function HomePage(props) {
   return (
     <div>
+      <Head>
+        <title>S.O.S Beauty - Салон за красота Пловдив</title>
+        <meta name="description" content="Салон за красота. Маникюр, педикюр, ноктопластика, кола маска, козметика, почистване на лице, миглопластика" />
+      </Head>
       <h1>Home Page</h1>
       <ServicesList services={props.loadedServices} />
     </div>
