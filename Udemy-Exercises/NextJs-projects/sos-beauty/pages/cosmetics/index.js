f42d8e48fb13f611acf365738510e67051a8a465
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-04-29 18:43:00 +0300

    feat: getting started on the cosmetics page - first testing

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/cosmetics/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/cosmetics/index.js
@@ -1,9 +1,31 @@
-function CosmeticsPage() {
+
+function CosmeticsPage(props) {
+    console.log(props.cosmetics)
     return (
         <div>
             <h1>Козметика</h1>
+            <ul>
+                
+            </ul>
         </div>
     )
+}
+
+export async function getServerSideProps() {
+    const response = await fetch("https://sos-beauty-f7b87-default-rtdb.europe-west1.firebasedatabase.app/services/cosmetics.json");
+    const cosmetics = await response.json();
+    // console.log("Cosmetics: ", cosmetics);
+    const cosmeticsArray = [];
+    for (let key in cosmetics) {
+        console.log(key, cosmetics[key].titleBig);
+        cosmeticsArray.push(cosmetics[key]);
+    }
+
+    return {
+        props: {
+            cosmetics: cosmeticsArray
+        }
+    }
 }
 
 export default CosmeticsPage;
