726e37a0e83f7cfc5ee1386c893ea92626cc8b80
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-05-06 08:15:54 +0300

    feat: unnecessary code from test targets has been removed

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/cosmetics/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/cosmetics/index.js
@@ -1,42 +1,9 @@
-import Link from "next/link";
-import Image from "next/image";
 import CosmeticsList from "../../components/cosmetics/cosmetics-list";
-
-
-// import ServicesList from "../../components/services/services-list";
 
 function CosmeticsPage(props) {
     return (
         <div>
             <h1>Козметика</h1>
-            {/* <ul>
-                {
-                    props.cosmetics.map(eachService => {
-                        return (
-                            <li key={eachService.id}>
-                                <div>
-                                    <div>
-                                        <h2>{eachService.titleBig}</h2>
-                                    </div>
-                                    <div>
-                                        <h3>{eachService.titleSmall}</h3>
-                                    </div>
-                                </div>
-                                <div>
-                                    <Image width={300} height={220} src={eachService.image} alt={eachService.titleBig} />
-                                </div>
-                                <div>
-                                    <Link href={`/${eachService.id}`}>
-                                        <a>Виж повече</a>
-                                    </Link>
-                                </div>
-                            </li>
-                        )
-                    })
-                }
-            </ul> */}
-            {/* <CosmeticsList cosmeticsObj={props.cosmetics} /> */}
-            {/* <ServicesList services={props.cosmetics} /> */}
             <CosmeticsList services={props.cosmetics} />
         </div>
     )
@@ -51,7 +18,6 @@ export async function getServerSideProps() {
         cosmetics[key].id = key;
         cosmeticsArray.push(cosmetics[key]);
     }
-    // console.log(cosmeticsArray)
 
     return {
         props: {
