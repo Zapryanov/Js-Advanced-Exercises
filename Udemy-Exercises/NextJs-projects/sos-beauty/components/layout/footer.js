e1cf1f859f5f02a4343ca013c3ee2510479c4ec5
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-05-06 21:18:38 +0300

    feat: sample footer added

---
+++ Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/footer.js
@@ -0,0 +1,13 @@
+// import Link from "next/link";
+
+import styles from "./footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles["footer"]}>
+            <p>Text from footer</p>
+        </footer>
+    )
+}
+
+export default Footer;
--- Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/layout.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/layout.js
@@ -1,3 +1,4 @@
+import Footer from "./footer";
 import MainHeader from "./main-header";
 
 function Layout(props) {
@@ -5,6 +6,7 @@ function Layout(props) {
         <>
             <MainHeader />
             <main>{props.children}</main>
+            <Footer />
         </>
     )
 }
