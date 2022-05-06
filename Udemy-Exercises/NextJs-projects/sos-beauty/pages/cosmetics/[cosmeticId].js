f1191455776a93274e573ad9b90d3ee16bf9d0f4
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-05-06 16:30:08 +0300

    feat: rough visualization of information

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/cosmetics/[cosmeticId].js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/cosmetics/[cosmeticId].js
@@ -1,19 +1,32 @@
 import Link from "next/link";
+import Image from "next/image";
 import { getCosmeticServiceById, getCosmeticsIds } from "../../data";
 import styles from "./[cosmeticId].module.css";
 
 function CurrentCosmeticService(props) {
 
     const currentService = props.currentCosmeticService;
+    const { titleBig, titleSmall, generalInfo, image, serviceDescription, price } = currentService;
 
     if (!currentService) {
         return <h1>Няма намерена такава услуга!</h1>
     }
 
     return (
-        <div>
-            <h1>Заглавие: {currentService.titleBig}</h1>
-            <div>Main Description</div>
+        <div className={styles["wrapper-current-cosmetic"]}>
+            <h2>Заглавие: {titleBig}</h2>
+            <ul className={styles["ul-service-description"]}>
+                {serviceDescription.map((sentence, i) => <li key={i}>{sentence}</li>)}
+            </ul>
+            <div className={styles["wrap-image"]}>
+                <Image width={600} height={400} src={image} alt={titleBig} />
+            </div>
+            <ul className={styles["ul-general-info"]}>
+                {generalInfo.map((sentence, i) => <li key={i} className={styles["li-general-info"]}>{sentence}</li>)}
+            </ul>
+            <div className={styles["wrap-price"]}>
+                <span className={styles.price}>Цена: {price}лв</span>
+            </div>
             <div className={styles["wrap-btn-cosmeticId"]}>
                 <Link href={`/cosmetics`}>
                     <a className={styles["btn-cosmeticId"]}>Назад</a>
