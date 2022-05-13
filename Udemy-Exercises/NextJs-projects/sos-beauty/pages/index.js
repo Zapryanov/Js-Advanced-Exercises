cd17fbd5e632e94d132e19cafb9a921b11f559ab
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-05-13 14:25:21 +0300

    feat: the H1 style has been corrected

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/index.js
@@ -3,6 +3,8 @@ import ServicesList from "../components/services/services-list";
 import Head from "next/head";
 import { getMainHeadings } from "../data";
 
+import styles from "./index.module.css";
+
 function HomePage(props) {
   return (
     <div>
@@ -10,7 +12,7 @@ function HomePage(props) {
         <title>S.O.S Beauty - Салон за красота Пловдив</title>
         <meta name="description" content="Салон за красота. Маникюр, педикюр, ноктопластика, кола маска, козметика, почистване на лице, миглопластика" />
       </Head>
-      <h1>Услуги</h1>
+      <h1 className={styles.pageHeading}>Услуги</h1>
       <ServicesList services={props.loadedServices} />
     </div>
   )
