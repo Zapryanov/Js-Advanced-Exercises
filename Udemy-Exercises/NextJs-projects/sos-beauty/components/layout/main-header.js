877bf307093aa8a31e9fe52a50932071309ac548
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-05-14 14:39:53 +0300

    feat: the menu and social connections are almost ready

--- Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/main-header.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/main-header.js
@@ -32,10 +32,17 @@ function MainHeader() {
                         </Link>
                     </li>
                     <li>
+                        <Link href="https://www.facebook.com/S.O.SBeauti/">
+                            <a className={styles["link-menu"]}  target="_blank">
+                                <Image className={styles["instagram-icon"]} width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1652520875/1200px-Instagram.svg_snthoj.png" alt="facebook" />
+                            </a>
+                        </Link>
+                    </li>
+                    <li>
                         <Link href="tel:+35976862629">
                             <a className={`${styles["link-menu"]} ${styles["link-phone"]}`}>
                                 <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1652476270/phone_nbsxsf.png" alt="phone" />
-                                <span>0876 862 629</span>
+                                <span className={styles.phone}>0876 862 629</span>
                             </a>
                         </Link>
                     </li>
