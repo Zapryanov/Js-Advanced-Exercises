fccbb8b4e7fcbf65b0f6b60ee0309325eff45e32
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-05-19 09:22:30 +0300

    feat: the main vision of the page is ready

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/manicure/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/manicure/index.js
@@ -1,7 +1,29 @@
-function Manicure() {
+import { getManicure } from "../../data";
+import PricesList from "../../components/prices/prices-list";
+
+import styles from "./index.module.css";
+
+function Manicure(props) {
+    const { manicure } = props;
+    console.log(manicure);
     return (
-        <div></div>
+        <div>
+            <h1 className={styles.pageHeading}>Маникюр</h1>
+            <article className={styles["wrap-price-list"]}>
+                <PricesList prices={manicure}/>
+            </article>
+        </div>
     )
+}
+
+export async function getServerSideProps() {
+    const manicure = await getManicure();
+
+    return {
+        props: {
+            manicure
+        }
+    }
 }
 
 export default Manicure;
