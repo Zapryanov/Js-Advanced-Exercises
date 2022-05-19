44122699c3052d550376fb0eb47e5635db30a72c
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-05-19 08:42:43 +0300

    feat: a several changes and one new file has been added

--- Udemy-Exercises/NextJs-projects/sos-beauty/components/prices/prices-item.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/components/prices/prices-item.js
@@ -1,10 +1,12 @@
+import styles from "./prices-item.module.css";
+
 function PricesItem(props) {
     const { id, part, price } = props;
 
     return (
-        <li key={id}>
-            <span>{part}</span>
-            <span>{price}</span>
+        <li key={id} className={styles["li-price-item"]}>
+            <span className={styles.part}>{part}</span>
+            <span className={styles.price}>{price} лв</span>
         </li>
     )
 }
--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/waxing/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/waxing/index.js
@@ -1,19 +1,21 @@
 import PricesList from "../../components/prices/prices-list";
 import { getWaxing } from "../../data";
 
+import styles from "./index.module.css";
+
 function WaxingPage(props) {
     const { men, women } = props.prices;
 
     return (
         <div>
-            <h1>Кола Маска</h1>
-            <article>
-                <section>
-                    <h2>Жени</h2>
+            <h1 className={styles.pageHeading}>Кола Маска</h1>
+            <article className={styles["wrap-women-and-men"]}>
+                <section className={styles["section-human"]}>
+                    <h2 className={styles["h2-waxing"]}>Жени</h2>
                     <PricesList prices={women}/>
                 </section>
-                <section>
-                    <h2>Мъже</h2>
+                <section className={styles["section-human"]}>
+                    <h2 className={styles["h2-waxing"]}>Мъже</h2>
                     <PricesList prices={men} />
                 </section>
             </article>
