150935994a09d3c9b612ab385c43d48948d6659d
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-05-25 23:04:23 +0300

    feat: new items added to aboutUs page

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/about/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/about/index.js
@@ -1,26 +1,55 @@
+import Image from "next/image";
+
+import { getManicureAboutUs } from "../../data";
 import styles from "./index.module.css";
 
-function About() {
+function About(props) {
+    const manicureImages = props.manicurePictures;
+
     return (
         <div>
             <h1 className="pageHeading">За нас</h1>
             <section className={styles["wrapper"]}>
                 <article className={styles["top-center"]}>
                     <p>
-                        Всяка жена е перфектна такава, каквато е. Салон за красота &rdquo;S0S-Beauty&rdquo; е тук, 
+                        Всяка жена е перфектна такава, каквато е. Салон за красота &rdquo;S.0.S Beauty&rdquo; е тук, 
                         за да ви помогнем да подобрите естествената си красота и увереност. Салонът е специализиран в изкуството 
                         на маникюр, педикюр, ноктопластика, кола маска, козметични процедури за лице, и миглопластика. Предлагаме
                         приятна и спокойна обстановка, където може да отделите време за себе си и да се поглезите. 
-                        Работим само с качествени, професионални продукти!
                     </p>
                 </article>
-                <article>
-                    <div>Text1</div>
-                    <div>Text2</div>
+                <article className={styles["wrap-text-and-images"]}>
+                    <div className={styles["wrap-text"]}>
+                        <p>
+                            &rdquo;S.O.S Beauty&rdquo; е вдъхновяващ салон за красота, където може да се поглезите. Едни от услугите в които 
+                            сме специализирани са ноктопластика, поставяне на гел лак, педикюр. Нашия салон предлага също луксозен педикюр,
+                            в допълнение с топла атмосфера е идеалното място, когато имате нужда от малко време за себе си! Не поставяме обикновенни лакове!
+                            Работим само с качествени професионални продукти! Ако търсите професионално обслужване и топла атмосфера, тук е вашето място, заповядайте!
+                        </p>
+                    </div>
+                    <div className={styles["wrap-images"]}>
+                        <ul className={styles["remove-space"]}>
+                            {manicureImages.map((image, i) => 
+                                <li className={styles["current-image"]} key={i}>
+                                    <Image className="img" width={200} height={150} src={image} alt="nails" />
+                                </li>
+                            )}
+                        </ul>
+                    </div>
                 </article>
             </section>
         </div>
     )
+}
+
+export async function getServerSideProps() {
+    const data = await getManicureAboutUs();
+
+    return {
+        props: {
+            manicurePictures: data
+        }
+    }
 }
 
 export default About;
