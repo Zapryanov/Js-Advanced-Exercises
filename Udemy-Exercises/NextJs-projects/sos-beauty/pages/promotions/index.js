bf4e44e1621289b1a7f349d83a4f72d2e24385ca
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-05-31 23:22:16 +0300

    feat: check if object is null or undefined

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/promotions/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/promotions/index.js
@@ -1,32 +1,55 @@
 import Image from "next/image";
+import { getPromotions } from "../../data";
 
 import styles from "./index.module.css";
 
-function PromotionsPage() {
-    return (
-        <div className={styles["wrapper-promotions"]}>
-            <h1 className="pageHeading">Промоции</h1>
-            <div className={styles["text-background"]}>
-                <p>-% Промоции -%</p>
+function PromotionsPage(props) {
+    if (props.promotions) {
+        const { descriptionMain, descriptionDiscount, image, priceDiscounted, priceRegular } = props.promotions;
+        return (
+            <div className={styles["wrapper-promotions"]}>
+                <h1 className="pageHeading">Промоции</h1>
+                <div className={styles["text-background"]}>
+                    <p>-% Промоции -%</p>
+                </div>
+                <section className={styles["section-promotions"]}>
+                    <h3>Гел лак</h3>
+                    <article>
+                        <div className={styles["wrap-image-promotions"]}>
+                            <Image className={styles["image"]} width={330} height={220} src={image} alt="image-promotions" />
+                        </div>
+                        <div className={styles["wrap-text-promotions"]}>
+                            <p className={styles["descriptionMain"]}>{descriptionMain}</p>
+                            <p className={styles["descriptionDiscount"]}>{descriptionDiscount}</p>
+                            <p className={styles["priceRegular"]}>{priceRegular}</p>
+                            <span className={styles["priceDiscounted"]}>{priceDiscounted}</span>
+                        </div>
+                    </article>
+                </section>
             </div>
-            <section className={styles["section-promotions"]}>
-                <article>
-                    <div className={styles["wrap-image-promotions"]}>
-                        {/* <Image width={450} height={300} src={} alt="image-promotions" /> */}
-                    </div>
-                    <div className={styles["wrap-text-promotions"]}>
-                        <p>
-                            Уважаеми клиенти, тази седмица от 30.05 до 05.06 включително, предлагаме на промоция с намаление поставяне на гел лак. 
-                            Материалите са на немската марка Nika Nagel. Nika Nagel е вече много години еталон за високо качество 
-                            и безопастност за здравето на естествените нокти.
-                        </p>
-                        <p>28% отстъпка</p>
-                        <p>При редовна цена от 35лв, сега може да се възползвате само за <span>25лв</span>!</p>
-                    </div>
-                </article>
-            </section>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className={styles["wrapper-promotions"]}>
+                <h1 className="pageHeading">Промоции</h1>
+                <div className={styles["text-background"]}>
+                    <p>-% Промоции -%</p>
+                </div>
+                <section className={styles["section-promotions"]}>
+                    <h3>Няма активна промоция!!!</h3>
+                </section>
+            </div>
+        )
+    }
+}
+
+export async function getServerSideProps() {
+    const promotionsObject = await getPromotions();
+     return {
+         props: {
+             promotions: promotionsObject
+         }
+     }
 }
 
 export default PromotionsPage;
