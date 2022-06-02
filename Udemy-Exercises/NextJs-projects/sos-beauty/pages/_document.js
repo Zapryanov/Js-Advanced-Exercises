f91929bd7afd3bf1bde4155f470a2006a18efed7
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-06-02 22:18:02 +0300

    feat: font-family added and unnecessary comments removed

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/_document.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"/>
+                <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,300&family=Pacifico&display=swap" rel="stylesheet"/>
                 </Head>
                 <body>
                     <Main />
