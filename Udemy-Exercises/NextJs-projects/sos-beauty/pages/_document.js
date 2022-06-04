429b690dd75a72616dc7ac8befdc42f17a4b8a1f
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-06-04 10:16:51 +0300

    feat: trying different types of fonts and installing DOMPurify

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/_document.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/_document.js
@@ -5,7 +5,10 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,300&family=Pacifico&display=swap" rel="stylesheet"/>
+                {/* <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,300&family=Pacifico&display=swap" rel="stylesheet"/> */}
+                {/* <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,300&family=Pacifico&family=Playfair+Display:ital,wght@1,400;1,500&display=swap" rel="stylesheet"/> */}
+                {/* <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Merriweather:wght@300;400&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,300&family=Pacifico&family=Playfair+Display:ital,wght@1,400;1,500&display=swap" rel="stylesheet"/> */}
+                <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Merriweather:wght@300;400&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,300&family=Open+Sans:wght@300;400;500&family=Pacifico&family=Playfair+Display:ital,wght@1,400;1,500&display=swap" rel="stylesheet"/>
                 </Head>
                 <body>
                     <Main />
