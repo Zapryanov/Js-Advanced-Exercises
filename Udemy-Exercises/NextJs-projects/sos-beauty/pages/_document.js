b576c59bccab14e4fb5ba18d86b4982fd67d2184
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-06-06 15:04:11 +0300

    feat: a small change with google fonts

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/_document.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400&family=Montserrat:wght@100;200;300;400;500;600&family=Pacifico&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=El+Messiri&family=Merriweather:wght@300;400&family=Montserrat:wght@100;200;300;400;500;600&family=Pacifico&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
