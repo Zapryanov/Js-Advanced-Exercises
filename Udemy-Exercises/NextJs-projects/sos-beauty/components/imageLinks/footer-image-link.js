629e07545e04ffbf7e7ebc08c426f593410d752d
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-06-17 07:30:13 +0300

    feat: the media query about footer is ready

--- Udemy-Exercises/NextJs-projects/sos-beauty/components/imageLinks/footer-image-link.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/components/imageLinks/footer-image-link.js
@@ -6,7 +6,7 @@ import styles from "./footer-image-link.module.css";
 function FooterImageLink({link, image}) {
     return (
         <Link href={link}>
-            <a target="_blank">
+            <a className={styles["wrap-image"]} target="_blank">
                 <Image className={styles.image} width={280} height={175} src={image} alt="google-map" />  
             </a>
         </Link>
