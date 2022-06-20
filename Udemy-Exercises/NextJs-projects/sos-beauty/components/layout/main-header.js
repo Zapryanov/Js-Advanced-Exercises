5da4442825182a2a10224536518db8057feb2c4c
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-06-20 20:35:14 +0300

    feat: header's media query is ready

--- Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/main-header.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/main-header.js
@@ -11,7 +11,7 @@ function MainHeader() {
                 <div className={styles.salon}>Beauty Salon</div>
             </div>
             <nav>
-                <ul className={styles.menu}>
+                <ul className={`${styles.menu} ${styles["main-menu"]}`}>
                     <li>
                         <Link href="/">
                             <a className={styles["link-menu"]}>Услуги</a>
@@ -33,17 +33,17 @@ function MainHeader() {
                         </Link>
                     </li>
                 </ul>
-                <ul className={styles.menu}>
+                <ul className={`${styles.menu} ${styles["socials-header"]}`}>
                     <li>
                         <Link href="https://www.facebook.com/S.O.SBeauti/">
-                            <a className={`${styles["link-menu"]} ${styles["facebook-icon"]}`}  target="_blank">
+                            <a className={`${styles["link-menu"]} ${styles["facebook-icon"]} ${styles["social-icons"]}`}  target="_blank">
                                 <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1655236128/facebook-icon-3d_szr1ix.png" alt="facebook" />
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href="https://www.facebook.com/S.O.SBeauti/">
-                            <a className={styles["link-menu"]}  target="_blank">
+                            <a className={`${styles["link-menu"]} ${styles["instagram-icon"]} ${styles["social-icons"]}`}  target="_blank">
                                 <Image className={styles["instagram-icon"]} width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1652521525/instagram-min_szc2ds.png" alt="facebook" />
                             </a>
                         </Link>
@@ -51,7 +51,9 @@ function MainHeader() {
                     <li>
                         <Link href="tel:+35976862629">
                             <a className={`${styles["link-menu"]} ${styles["link-phone"]}`}>
-                                <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1652476270/phone_nbsxsf.png" alt="phone" />
+                                <span className={styles["phone-image"]}>
+                                    <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1652476270/phone_nbsxsf.png" alt="phone" />
+                                </span>
                                 <span className={styles.phone}>0876 862 629</span>
                             </a>
                         </Link>
