9a17d5e03c29d3dc6339447e7a995d8267b7b417
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-06-28 23:56:39 +0300

    feat: fetching data and css file added

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/microblading/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/microblading/index.js
@@ -1,10 +1,42 @@
-function Microblading() {
+import Image from "next/image";
+import styles from "./index.module.css";
+
+import PricesList from "../../components/prices/prices-list";
+import { getMicroblading } from "../../data";
+
+function Microblading(props) {
+    const microblading = props.microblading;
 
     return (
         <div>
             <h1 className="pageHeading">Микроблейдинг</h1>
+            <article className={styles["wrap-microblading"]}>
+                <section className={styles["image-section"]}>
+                    <div className={styles["wrap-image"]}>
+                        <Image width={500} height={350} src={microblading.image} alt="microblading" />
+                    </div>
+                </section>
+                <section className={styles["text-section"]}>
+                    <p className={styles["wrap-text"]}>
+                        {microblading.description}
+                    </p>
+                </section>
+                <section className="wrap-price-list">
+                    <PricesList prices={microblading.prices}/>
+                </section>
+            </article>
         </div>
     )
+}
+
+export async function getServerSideProps() {
+    const microbladingInfo = await getMicroblading();
+
+    return {
+        props: {
+            microblading: microbladingInfo
+        }
+    }
 }
 
 export default Microblading;
