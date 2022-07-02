d8fd9da58495a272ac8ad405dba5e3e4d641181f
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-07-03 00:16:37 +0300

    feat: dalyFaceTreatments page is almost ready now working on media query

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/dalyFacialTreatments/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/dalyFacialTreatments/index.js
@@ -1,4 +1,5 @@
 import { getDalyFaceTreatments } from "../../data";
+import styles from "./index.module.css";
 
 function DalyFacialTreatments(props) {
     const { dalyFacialTreatments } = props;
@@ -7,10 +8,14 @@ function DalyFacialTreatments(props) {
         <section>
             <h1 className="pageHeading">Ежедневни процедури за лице</h1>
             {dalyFacialTreatments.map((faceService, i) => (
-                <article key={i}>
+                <article className={styles["each-facial-treatment"]} key={i}>
                     <h3>{faceService[0]}</h3>
-                    <div>{faceService[1].description}</div>
-                    <div>{faceService[1].price}</div>
+                    <div className={styles.description}>{faceService[1].description}</div>
+                    <div className={styles["wrap-face-treatments-price"]}>
+                        <span className={styles["word-price"]}>Цена</span>
+                        <span className={styles.dots}>............................................................................................................................................................................................................................................................................................................................</span>
+                        <span className={styles.price}>{faceService[1].price} лв</span>
+                    </div>
                 </article>
             ))}
         </section>
