33b16c80902bd1fa47b0fadd5b1bf66a9d5f623b
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-07-07 17:51:46 +0300

    feat: testing showing brands logos

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/about/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/about/index.js
@@ -13,12 +13,34 @@ function About(props) {
             <h2 className="pageHeading">За нас</h2>
             <section className={styles["wrapper"]}>
                 <article className={styles["top-center"]}>
-                    <p>
-                        Всяка жена е перфектна такава, каквато е. Салон за красота &rdquo;S.0.S Beauty&rdquo; е тук, за да ви помогнем да 
-                        подобрите естествената си красота и увереност. Салонът е специализиран в изкуството на маникюр, педикюр, ноктопластика, 
-                        кола маска, козметични процедури за лице, миглопластика и микроблейдинг. Работим само с качествени професионални
-                        продукти. Насладете са на вълшебството на красотата в стилна обстановка, приветлива атмосфера и професионализъм.
-                    </p>
+                    <div className={styles["main-description"]}>
+                        <p>
+                            Всяка жена е перфектна такава, каквато е. Салон за красота &rdquo;S.0.S Beauty&rdquo; е тук, за да ви помогнем да 
+                            подобрите естествената си красота и увереност. Салонът е специализиран в изкуството на маникюр, педикюр, ноктопластика, 
+                            кола маска, козметични процедури за лице, миглопластика и микроблейдинг. Работим само с качествени професионални
+                            продукти. Насладете са на вълшебството на красотата в стилна обстановка, приветлива атмосфера и професионализъм.
+                        </p>
+                    </div>
+                    <div className={styles["wrap-images-brands"]}>
+                        <div>
+                            <Image width={100} height={100} src="https://res.cloudinary.com/audipower/image/upload/v1657142705/nika-nagel-logo-original_qhptoi.png" alt="nika-nagel-logo" />
+                        </div>
+                        <div>
+                            <Image width={300} height={100} src="https://res.cloudinary.com/audipower/image/upload/v1657143061/iroha-logo-min_pfwpfc.png" alt="iroha-logo" />
+                        </div>
+                        <div>
+                            <Image width={240} height={100} src="https://res.cloudinary.com/audipower/image/upload/v1657204745/dr-renaud-rectangle-logo-min_jsyxsz.png" alt="iroha-logo" />
+                        </div>
+                        {/* <div className={styles["wrapper-dr-renaud-logo"]}>
+                            <div className={styles["dr-renaud-logo"]}>
+                                <div className={styles["blue-circle"]}>DR</div>
+                                <div className={styles["name"]}>Renaud</div>
+                            </div>
+                        </div> */}
+                        <div>
+                            <Image width={150} height={100} src="https://res.cloudinary.com/audipower/image/upload/v1657142097/termish-logo-min_nwvvit.png" alt="termish-logo" />
+                        </div>
+                    </div>
                 </article>
                 {objEntries.map((eachService, i) => {
                     if (i % 2 === 0) {
