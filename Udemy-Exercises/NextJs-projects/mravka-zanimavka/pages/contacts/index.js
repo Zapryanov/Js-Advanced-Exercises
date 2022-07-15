59f101a8223116f3e8a15bf4e3f28fb3866b2da9
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-07-15 14:05:44 +0300

    feat: unnecessary code removed

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/contacts/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/contacts/index.js
@@ -2,21 +2,9 @@ import Image from "next/image";
 import styles from "./index.module.css";
 import Head from "next/head";
 
-// import GoogleMapReact from 'google-map-react';
 import Link from "next/link";
 
-// const AnyReactComponent = ({ text }) => <div className={styles.googleText}>{text}</div>;
-
 function ContactsPage() {
-
-    // const renderMarkers = (map, maps) => {
-    //     let lessons = new maps.Marker({
-    //         position: { lat: Number(process.env.lat), lng: Number(process.env.lng) },
-    //         map,
-    //         title: `mravka-zanimavka`
-    //     });
-    //     return lessons;
-    // };
 
     return (
         <>
@@ -56,20 +44,6 @@ function ContactsPage() {
                 </section>
                 <section>
                     <div className={`${styles["wrap-google-maps"]} ${styles.maps}`}>
-                        {/* <GoogleMapReact
-                            bootstrapURLKeys={{ key: process.env.bootstrapURLKeys }}
-                            defaultCenter={{ lat: Number(process.env.lat), lng: Number(process.env.lng) }}
-                            defaultZoom={17}
-                            yesIWantToUseGoogleMapApiInternals
-                            onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
-                        >
-                            <AnyReactComponent
-                                lat={Number(process.env.lat)}
-                                lng={Number(process.env.lng)}
-                                text="Мравка Занимавка"
-                            />
-                        </GoogleMapReact> */}
-
                         <div>
                             <Link href="https://www.google.com/maps/place/%D1%83%D0%BB.+%E2%80%9E%D0%99%D0%BE%D0%B0%D0%BA%D0%B8%D0%BC+%D0%93%D1%80%D1%83%D0%B5%D0%B2%E2%80%9C+15%D0%90,+4000+%D0%9A%D0%B0%D0%BF%D0%B0%D0%BD%D0%B0,+%D0%9F%D0%BB%D0%BE%D0%B2%D0%B4%D0%B8%D0%B2/@42.1504037,24.7471568,19z/data=!3m1!4b1!4m5!3m4!1s0x14acd1bd12ab4ecd:0x47a3f048ab238e28!8m2!3d42.1504037!4d24.747704">
                                 <a  className={styles["google-big-link"]} target="_blank">
@@ -77,7 +51,6 @@ function ContactsPage() {
                                 </a>
                             </Link>
                         </div>
-
                     </div>
                 </section>
             </div>
