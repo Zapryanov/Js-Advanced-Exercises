acf93c190bb70fc27b87941b3f65b4eafbc9a274
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-07-22 10:34:02 +0300

    feat: a few changes for better UI

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/contacts/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/pages/contacts/index.js
@@ -60,7 +60,7 @@ function ContactsPage(props) {
                             </Link>
                             <Link href="https://www.google.com/maps/place/%D1%83%D0%BB.+%E2%80%9E%D0%99%D0%BE%D0%B0%D0%BA%D0%B8%D0%BC+%D0%93%D1%80%D1%83%D0%B5%D0%B2%E2%80%9C+15%D0%90,+4000+%D0%9A%D0%B0%D0%BF%D0%B0%D0%BD%D0%B0,+%D0%9F%D0%BB%D0%BE%D0%B2%D0%B4%D0%B8%D0%B2/@42.1504037,24.7471568,19z/data=!3m1!4b1!4m5!3m4!1s0x14acd1bd12ab4ecd:0x47a3f048ab238e28!8m2!3d42.1504037!4d24.747704">
                                 <a className={styles["google-big-link"]} target="_blank">
-                                    <Image width={500} height={500} src="https://res.cloudinary.com/audipower/image/upload/v1657875981/mravka-min_asltxa.jpg" alt="google-map" />
+                                    <Image width={500} height={471} src="https://res.cloudinary.com/audipower/image/upload/v1657875981/mravka-min_asltxa.jpg" alt="google-map" />
                                 </a>
                             </Link>
                         </div>
