8ca36e851d9ae96163675f7ea5448769baf9415b
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-07-31 17:13:15 +0300

    feat: smaller icons added

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/layout/footer.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/layout/footer.js
@@ -11,7 +11,7 @@ function Footer() {
                     <div>
                         <Link href="https://www.facebook.com/MravkaZanimavka">
                             <a className={`${styles["position-icon"]} ${styles["facebook-link"]}`} target="_blank">
-                                <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1655233946/facebook-logo_glwf64.png" alt="facebook-icon" />
+                                <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1659217536/facebook_r6r45d.png" alt="facebook-icon" />
                                 <span className={styles["space-left"]}>Мравка Занимавка</span>
                             </a>
                         </Link>
@@ -21,7 +21,7 @@ function Footer() {
                     <div>
                         <Link href="/contacts">
                             <a className={`${styles["position-icon"]} ${styles["facebook-link"]}`}>
-                                <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1652476270/phone_nbsxsf.png" alt="contacts" />
+                                <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1659217349/phone_kb7ijk.png" alt="contacts" />
                                 <span className={styles["space-left"]}>Контакти</span>
                             </a>
                         </Link>
