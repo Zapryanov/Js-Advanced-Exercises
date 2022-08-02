e22854cbbecf6c9287ac2efd16841dc73622bc49
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-08-02 11:00:00 +0300

    feat: image dimension resized and lower size image added

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/big-button/index.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/big-button/index.js
@@ -5,7 +5,7 @@ function BigButton() {
     return (
         <div className={styles["leaf-effect"]}>
             <a href="https://www.facebook.com/MravkaZanimavka" target="__blank">
-                <Image width={1200} height={200} src="https://res.cloudinary.com/audipower/image/upload/v1643362891/save-lesson_izgw4s.png" alt="link-facebook" />
+                <Image width={1123} height={200} src="https://res.cloudinary.com/audipower/image/upload/v1659386267/save-lesson-min_fcrlay.png" alt="link-facebook" />
             </a>
         </div>
     )
