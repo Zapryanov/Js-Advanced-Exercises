64970bdbd01d7a888f801a8bbb7ff9c47c2e9c62
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-08-09 09:53:34 +0300

    feat: smaller image added

--- Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/layout/header.js
+++ Udemy-Exercises/NextJs-projects/mravka-zanimavka/components/layout/header.js
@@ -9,7 +9,7 @@ function Header() {
             <div className={styles.logo}>
                 <Link href="/">
                     <a>
-                        <Image width={150} height={150} src="https://res.cloudinary.com/audipower/image/upload/v1643362514/logo_fusle7.png" alt="logo" />
+                        <Image width={150} height={150} src="https://res.cloudinary.com/audipower/image/upload/v1659386249/logo-min_xzllks.png" alt="logo" />
                     </a>
                 </Link>
             </div>
