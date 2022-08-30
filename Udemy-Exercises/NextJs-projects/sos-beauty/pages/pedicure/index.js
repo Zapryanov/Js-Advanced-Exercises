575f12cef97821f48d4e29edb0a9408f4d1e724c
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-08-30 23:12:33 +0300

    feat: Head added in pedicure page

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/pedicure/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/pedicure/index.js
@@ -1,12 +1,16 @@
+import Head from "next/head";
+
 import PricesList from "../../components/prices/prices-list";
 import { getPedicure } from "../../data";
-
-import styles from "./index.module.css";
 
 function Pedicure(props) {
     const { pedicure } = props;
     return (
         <div>
+            <Head>
+                <title>Педикюр | салон за красота S.O.S-Beauty</title>
+                <meta name="description" content="Педикюр с лак или гел лак в Пловдив, Тракия, до Акваленд" />
+            </Head>
             <h2 className="pageHeading">Педикюр</h2>
             <article className="wrap-price-list">
                 <PricesList prices={pedicure}/>
