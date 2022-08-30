87e3f0b5b4f65afb8ad89cc738a9f889a10847aa
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-08-30 19:11:43 +0300

    feat: Head added in hairdressing

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/hairdressing/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/hairdressing/index.js
@@ -1,11 +1,16 @@
+import Head from "next/head";
 import PricesList from "../../components/prices/prices-list";
 import { getHairdressing } from "../../data";
 
 function Hairdressing(props) {
     const { hairdressing } = props;
-    console.log(hairdressing)
+
     return (
         <div>
+            <Head>
+                <title>Фризьорство | салон за красота S.O.S-Beauty</title>
+                <meta name="description" content="Фризьорство, подстригване (мъжко, дамско, детско), прически в Пловдив, Тракия, до Акваленд" />
+            </Head>
             <h2 className="pageHeading">Фризьорство</h2>
             <article className="wrap-price-list">
                 <PricesList prices={hairdressing} />
