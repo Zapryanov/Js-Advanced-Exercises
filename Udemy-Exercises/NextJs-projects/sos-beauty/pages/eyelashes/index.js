6d7031bed96af2fdaa9667c1df0d6423304bfae4
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-08-30 19:01:44 +0300

    feat: Head added in eyelashes

--- Udemy-Exercises/NextJs-projects/sos-beauty/pages/eyelashes/index.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/pages/eyelashes/index.js
@@ -1,12 +1,16 @@
+import Head from "next/head";
+
 import PricesList from "../../components/prices/prices-list";
 import { getEyelashes } from "../../data";
-
-import styles from "./index.module.css";
 
 function Pedicure(props) {
     const { eyelashes } = props;
     return (
         <div>
+            <Head>
+                <title>Миглопластика | салон за красота S.O.S-Beauty</title>
+                <meta name="description" content="Миглопластика - поставяне на мигли в Пловдив, Тракия, до Акваленд" />
+            </Head>
             <h2 className="pageHeading">Миглопластика</h2>
             <article className="wrap-price-list">
                 <PricesList prices={eyelashes}/>
