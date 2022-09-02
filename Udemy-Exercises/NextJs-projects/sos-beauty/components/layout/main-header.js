a3764faa92038cf2e123596a97dc1a65e232a39e
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-09-02 07:59:41 +0300

    feat: two social logos added in images folder

--- Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/main-header.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/main-header.js
@@ -37,22 +37,22 @@ function MainHeader() {
                     <li>
                         <Link href="https://www.facebook.com/S.O.SBeauti/">
                             <a className={`${styles["link-menu"]} ${styles["facebook-icon"]} ${styles["social-icons"]}`}  target="_blank">
-                                <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1655237973/facebook-icon-3d-new_aqvxyn.png" alt="facebook" />
+                                <Image width={20} height={20} src="/images/facebook-logo-3d.png" alt="facebook" />
                             </a>
                         </Link>
                     </li>
                     <li>
                         <Link href="https://www.facebook.com/S.O.SBeauti/">
                             <a className={`${styles["link-menu"]} ${styles["instagram-icon"]} ${styles["social-icons"]}`}  target="_blank">
-                                <Image className={styles["instagram-icon"]} width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1652521525/instagram-min_szc2ds.png" alt="facebook" />
+                                <Image className={styles["instagram-icon"]} width={20} height={20} src="/images/instagram-logo.png" alt="instagram" />
                             </a>
                         </Link>
                     </li>
                     <li>
-                        <Link href="tel:+35976862629">
+                        <Link href="tel:+359876862629">
                             <a className={`${styles["link-menu"]} ${styles["link-phone"]} ${styles["social-icons"]}`}>
                                 <span className={styles["phone-image"]}>
-                                    <Image width={20} height={20} src="https://res.cloudinary.com/audipower/image/upload/v1652476270/phone_nbsxsf.png" alt="phone" />
+                                    <Image width={20} height={20} src="/images/phone.png" alt="phone" />
                                 </span>
                                 <span className={styles.phone}>0876 862 629</span>
                             </a>
