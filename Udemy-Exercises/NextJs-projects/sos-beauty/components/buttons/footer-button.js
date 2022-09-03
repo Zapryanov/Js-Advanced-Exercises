3910dc60af37124e9909f10229c13413e22faa25
Ivan Zapryanov <50863199+Zapryanov@users.noreply.github.com>
2022-09-03 08:17:19 +0300

    feat: optimizing code in footer

--- Udemy-Exercises/NextJs-projects/sos-beauty/components/buttons/footer-button.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/components/buttons/footer-button.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import styles from "./footer-button.module.css";
 
-function FooterButton({direction, link}) {
+function FooterButton({link}) {
     return (
-        <div className={`${styles["see-more"]} ${direction}`}>
+        <div className={styles["see-more"]}>
             <div className={styles["inset-shadow"]}>
                 <Link href={link}>
                     <a className={styles["see-more-text"]} target="_blank">виж повече</a>
--- Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/footer.js
+++ Udemy-Exercises/NextJs-projects/sos-beauty/components/layout/footer.js
@@ -7,9 +7,7 @@ function Footer() {
         googleLink: "https://www.google.com/maps/place/S.O.S+Beauty/@42.137649,24.7864931,19z/data=!4m5!3m4!1s0x14acd1671feabfcf:0xbe40cf6f6dab0e16!8m2!3d42.137649!4d24.7870403",
         googleImage: "/images/google-map-smallest.jpg",
         facebookLink: "https://www.facebook.com/S.O.SBeauti",
-        facebookImage: "/images/facebook.jpg",
-        left: styles.left,
-        right: styles.right
+        facebookImage: "/images/facebook.jpg"
     }
 
     return (
@@ -30,14 +28,14 @@ function Footer() {
             <div className={`${styles["each-footer-block"]}`}>
                 <h3>Google Map</h3>
                 <div className={styles["wrap-socials"]}>
-                    <FooterButton direction={obj.left} link={obj.googleLink} />
+                    <FooterButton className={styles.left} link={obj.googleLink} />
                     <FooterImageLink link={obj.googleLink} image={obj.googleImage} />
                 </div>
             </div>
             <div className={`${styles["each-footer-block"]}`}>
                 <h3>Facebook</h3>
                 <div className={styles["wrap-socials"]}>
-                    <FooterButton  direction={obj.left} link={obj.facebookLink} />
+                    <FooterButton  className={styles.left} link={obj.facebookLink} />
                     <FooterImageLink link={obj.facebookLink} image={obj.facebookImage} />
                 </div>
             </div>
